7d013bd6dd1eb31916105eecfe25fedcc7f6b4f1
hashimotoryoma <ryoma@hashimotoryoumanoMacBook-Pro.local>
2020-03-26 12:31:03 +0900

    add a new file

---
+++ DataStructure/UnionFind.hpp
@@ -0,0 +1,23 @@
+#ifndef call_from_test
+#include <bits/stdc++.h>
+using namespace std;
+#endif
+
+struct UnionFind {
+    vector<int> par;
+    UnionFind(int size) : par(size, -1) {}
+    bool unionSet(int x, int y) {
+        x = root(x);
+        y = root(y);
+        if(x != y) {
+            if(par[y] < par[x])
+                swap(x, y);
+            par[x] += par[y];
+            par[y] = x;
+        }
+        return x != y;
+    }
+    bool same(int x, int y) { return root(x) == root(y); }
+    int root(int x) { return par[x] < 0 ? x : par[x] = root(par[x]); }
+    int size(int x) { return -par[root(x)]; }
+};

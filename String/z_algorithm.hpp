e93f685fcc8b767319602bd45b6f1c463a26e340
hashimotoryoma <ryoma@hashimotoryoumanoMacBook-Pro.local>
2020-03-26 16:32:30 +0900

    add a new file

---
+++ String/z_algorithm.hpp
@@ -0,0 +1,27 @@
+/**
+ * @title Z_algorithm
+ * @brief $prefix_i$ は、$S$ と $S.substr(i)$ の LCP(longest common prefix)
+ * @brief O(N)
+ */
+
+#ifndef call_from_test
+#include <bits/stdc++.h>
+using namespace std;
+#endif
+
+vector<int> z_algorithm(const string &s) {
+    vector<int> prefix(s.size());
+    for(int i = 1, j = 0; i < s.size(); i++) {
+        if(i + prefix[i - j] < j + prefix[j]) {
+            prefix[i] = prefix[i - j];
+        } else {
+            int k = max(0, j + prefix[j] - i);
+            while(i + k < s.size() && s[k] == s[i + k])
+                ++k;
+            prefix[i] = k;
+            j = i;
+        }
+    }
+    prefix[0] = (int)s.size();
+    return prefix;
+}

9e5f237c3cd249cdf15bd1e86941b15e80b813ef
hashimotoryoma <ryoma@hashimotoryoumanoMacBook-Pro.local>
2020-04-08 18:31:13 +0900

    add yukicoder

---
+++ test/yukicoder/1973.test.cpp
@@ -0,0 +1,23 @@
+#define PROBLEM "https://yukicoder.me/problems/1973"
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define call_from_test
+#include "Math/FormalPowerSeries.hpp"
+#include "Math/Kitamasa.hpp"
+#undef call_from_test
+
+signed main() {
+  cin.tie(0);
+  ios::sync_with_stdio(0);
+  int Q;
+  cin >> Q;
+  while (Q--) {
+    long long n;
+    cin >> n;
+    cout << kitamasa({1, 1, 1, 1}, {0, 0, 0, 1}, n - 1, 17) << "\n";
+  }
+  cout << flush;
+  return 0;
+}

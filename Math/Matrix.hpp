df73236a75c3e369fc799d1a6707b0d100675f5e
hashimotoryoma <ryoma@hashimotoryoumanoMacBook-Pro.local>
2020-04-23 00:59:14 +0900

    modify matrix

--- Math/Matrix.hpp
+++ Math/Matrix.hpp
@@ -99,8 +99,7 @@ struct Matrix {
       for (int j = 0; j < m; j++) c[i][j] = a[i][j];
     for (int i = 0; i < n; i++)
       for (int j = 0; j < l; j++) c[i][j + m] = b[i][j];
-    int d = 0;
-    for (int j = 0; j < m; j++) {
+    for (int j = 0, d = 0; j < m && d < n; j++) {
       int p = d;
       for (int i = d + 1; i < n; i++)
         if (compare(c[p][j], c[i][j])) p = i;

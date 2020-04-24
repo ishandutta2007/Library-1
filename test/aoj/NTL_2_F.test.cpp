4b870e66631a7a33b02f64f19819e588dfb5a588
hashimotoryoma <ryoma@hashimotoryoumanoMacBook-Pro.local>
2020-04-24 22:39:23 +0900

    add bigint

---
+++ test/aoj/NTL_2_F.test.cpp
@@ -0,0 +1,18 @@
+#define PROBLEM \
+  "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=NTL_2_F"
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define call_from_test
+#include "Math/BigInt.hpp"
+#undef call_from_test
+
+signed main() {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  BigInt A, B;
+  cin >> A >> B;
+  cout << A * B << endl;
+  return 0;
+}

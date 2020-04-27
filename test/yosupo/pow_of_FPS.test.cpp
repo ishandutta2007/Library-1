2c7efd818d81623048049c2bc847f5ccb0572acb
hashimotoryoma <ryoma@hashimotoryoumanoMacBook-Pro.local>
2020-04-27 15:19:38 +0900

    add powFPStest

---
+++ test/yosupo/pow_of_FPS.test.cpp
@@ -0,0 +1,26 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/pow_of_formal_power_series"
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define call_from_test
+#include "Math/FormalPowerSeries.hpp"
+#include "Math/ModInt.hpp"
+#undef call_from_test
+
+signed main() {
+  cin.tie(0);
+  ios::sync_with_stdio(0);
+  int N, M;
+  cin >> N >> M;
+  using Mint = ModInt<998244353>;
+  using FPS = FormalPowerSeries<Mint>;
+  FPS a(N);
+  for (int i = 0; i < N; i++) cin >> a[i];
+  auto b = a.pow(M);
+  for (int i = 0; i < N; i++) {
+    cout << b[i] << (i < N - 1 ? " " : "\n");
+  }
+  cout << flush;
+  return 0;
+}

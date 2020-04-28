35c50229bc03c28782f98ef557b3cdd2a789adaa
hashimotoryoma <ryoma@hashimotoryoumanoMacBook-Pro.local>
2020-04-28 23:59:27 +0900

    add BITtest

---
+++ test/yukicoder/649.BIT.test.cpp
@@ -0,0 +1,45 @@
+#define PROBLEM "https://yukicoder.me/problems/no/649"
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define call_from_test
+#include "DataStructure/BinaryIndexedTree.hpp"
+#undef call_from_test
+
+signed main() {
+  cin.tie(0);
+  ios::sync_with_stdio(0);
+  int Q, K;
+  cin >> Q >> K;
+  vector<long long> query;
+  vector<long long> x;
+  while (Q--) {
+    long long v;
+    cin >> v;
+    if (v == 1) {
+      cin >> v;
+      x.push_back(v);
+    } else {
+      v = -1;
+    }
+    query.push_back(v);
+  }
+  sort(x.begin(), x.end());
+  x.erase(unique(x.begin(), x.end()), x.end());
+  BinaryIndexedTree bit(x.size());
+  for (auto q : query) {
+    if (q < 0) {
+      int i = bit.lower_bound(K);
+      if (i >= 0) {
+        cout << x[i] << endl;
+        bit.add(i, -1);
+      } else {
+        cout << -1 << endl;
+      }
+    } else {
+      int i = lower_bound(x.begin(), x.end(), q) - x.begin();
+      bit.add(i, 1);
+    }
+  }
+}

ea8ce5347d7d93fbe0164f25842135466a4c888b
hashimotoryoma <ryoma@hashimotoryoumanoMacBook-Pro.local>
2020-04-29 01:45:15 +0900

    modify UF_po

--- DataStructure/UnionFind_Potential.hpp
+++ DataStructure/UnionFind_Potential.hpp
@@ -25,8 +25,9 @@ struct UnionFind_Potential {
   bool same(int u, int v) { return root(u) == root(v); }
   int root(int u) {
     if (par[u] < 0) return u;
+    int r = root(par[u]);
     val[u] += val[par[u]];
-    return par[u] = root(par[u]);
+    return par[u] = r;
   }
   int size(int u) { return -par[root(u)]; }
   Weight potential(int u) {

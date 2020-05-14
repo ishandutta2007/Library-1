0a108b9bd261b7dbe8a268990f6468232d3854c7
hashimotoryoma <ryoma@hashimotoryoumanoMacBook-Pro.local>
2020-05-14 21:58:30 +0900

    add MSA,SCC

--- Graph/MinimumSpanningAborescense.hpp
+++ Graph/MinimumSpanningAborescense.hpp
@@ -19,10 +19,10 @@ using namespace std;
 template <typename T>
 struct MinimumSpanningAborescense {
   struct Edge {
-    int src, dst;
+    int src, dst, id;
     T weight;
     Edge() {}
-    Edge(int s, int d, int w) : src(s), dst(d), weight(w) {}
+    Edge(int s, int d, int i, int w) : src(s), dst(d), id(i), weight(w) {}
   };
 
  private:
@@ -33,33 +33,40 @@ struct MinimumSpanningAborescense {
  public:
   MinimumSpanningAborescense(int sz) : n(sz) {}
   void add_edge(int src, int dst, T weight) {
-    edges.emplace_back(src, dst, weight);
+    edges.emplace_back(src, dst, edges.size(), weight);
   }
   pair<T, vector<int>> get_MSA(int root) {
     UnionFind uf(n);
     auto comp
         = [](const Edge &a, const Edge &b) { return a.weight > b.weight; };
     auto g = [](const Edge &a, const T &b) {
-      return Edge(a.src, a.dst, a.weight + b);
+      return Edge(a.src, a.dst, a.id, a.weight + b);
     };
     vector<Heap> heap(n, Heap(comp, g));
     for (auto &e : edges) heap[e.dst].push(e);
     T score = 0;
-    vector<int> seen(n, -1);
+    int m = edges.size();
+    vector<int> seen(n, -1), paredge(m), ei, leaf(n, -1), par(n), usede(m);
     seen[root] = root;
-    vector<int> par(n, -1);
-    par[root] = root;
     for (int s = 0; s < n; ++s) {
-      vector<int> path;
+      vector<int> path, ch;
+      int cyc = 0;
       for (int u = s; seen[u] < 0;) {
         path.push_back(u);
         seen[u] = s;
         if (heap[u].empty()) return {-1, vector<int>()};
         Edge min_e = heap[u].top();
-        par[min_e.dst] = min_e.src;
         score += min_e.weight;
         heap[u].add(-min_e.weight);
         heap[u].pop();
+        ei.push_back(min_e.id);
+        if (leaf[min_e.dst] == -1) leaf[min_e.dst] = min_e.id;
+        while (cyc) {
+          paredge[ch.back()] = min_e.id;
+          ch.pop_back();
+          cyc--;
+        }
+        ch.push_back(min_e.id);
         int v = uf.root(min_e.src);
         if (seen[v] == s) {
           Heap new_heap(comp, g);
@@ -67,12 +74,24 @@ struct MinimumSpanningAborescense {
             int w = path.back();
             path.pop_back();
             new_heap.merge(heap[w]);
+            cyc++;
             if (!uf.unite(v, w)) break;
           }
           heap[uf.root(v)] = new_heap;
           seen[uf.root(v)] = -1;
         }
         u = uf.root(v);
+      }
+    }
+    reverse(ei.begin(), ei.end());
+    par[root] = root;
+    for (auto i : ei) {
+      if (usede[i]) continue;
+      par[edges[i].dst] = edges[i].src;
+      int x = leaf[edges[i].dst];
+      while (x != i) {
+        usede[x] = 1;
+        x = paredge[x];
       }
     }
     return {score, par};
--- test/aoj/2647.test.cpp
+++ test/aoj/2647.test.cpp
@@ -15,18 +15,19 @@ signed main() {
   int N, M;
   cin >> N >> M;
   MinimumSpanningAborescense<int> graph(N);
-  bool unable[N] = {};
+  bool able[N];
+  fill(able, able + N, true);
   for (int i = 0; i < M; i++) {
     int a, b;
     cin >> a >> b;
-    unable[b] = true;
+    able[b] = false;
     graph.add_edge(a, b, 0);
     graph.add_edge(b, a, 1);
   }
   int min_cost = INT_MAX;
   vector<int> capitals;
   for (int i = 0; i < N; i++)
-    if (!unable[i]) {
+    if (able[i]) {
       int cost = graph.get_MSA(i).first;
       if (cost < min_cost) {
         min_cost = cost;

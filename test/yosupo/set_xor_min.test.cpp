2f4b26e35c3b939937033a3487a204dac9bedadc
hashimotoryoma <ryoma@hashimotoryoumanoMacBook-Pro.local>
2020-06-11 23:36:06 +0900

    add test of binarytrie

---
+++ test/yosupo/set_xor_min.test.cpp
@@ -0,0 +1,29 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/set_xor_min"
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define call_from_test
+#include "DataStructure/SegmentTree_Dynamic.hpp"
+#include "DataStructure/BinaryTrie.hpp"
+#undef call_from_test
+
+signed main() {
+  cin.tie(0);
+  ios::sync_with_stdio(0);
+  BinaryTrie S;
+  int Q;
+  cin >> Q;
+  while (Q--) {
+    int op, x;
+    cin >> op >> x;
+    if (op == 0) {
+      if (!S.count(x)) S.insert(x);
+    } else if (op == 1) {
+      if (S.count(x)) S.erase(x);
+    } else {
+      cout << (S.min_element(x) ^ x) << endl;
+    }
+  }
+  return 0;
+}

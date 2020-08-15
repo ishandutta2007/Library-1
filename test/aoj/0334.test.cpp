c5cef7a80100aea2796de781f92149e3e81f9543
hashiryo <ryoma@hashimotoryoumanoMacBook-Pro.local>
2020-08-15 16:22:18 +0900

    modify

---
+++ test/aoj/0334.test.cpp
@@ -0,0 +1,31 @@
+#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=0334"
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define call_from_test
+#include "src/Graph/MatchingBipartite.hpp"
+#undef call_from_test
+
+signed main() {
+  cin.tie(0);
+  ios::sync_with_stdio(0);
+  int N;
+  cin >> N;
+  N--;
+  MatchingBipartite graph(N, N);
+  for (int i = 0; i < N; i++)
+    for (int j = 0; j < N; j++) {
+      int b;
+      cin >> b;
+      if (b) graph.add_edge(j, i);
+    }
+  auto ans = graph.lexicographically_matching();
+  if (ans.first < N)
+    cout << "no" << endl;
+  else {
+    cout << "yes" << endl;
+    for (int i = 0; i < N; i++) cout << ans.second.first[i] + 1 << endl;
+  }
+  return 0;
+}
---
+++ test/aoj/2423.test.cpp
@@ -0,0 +1,36 @@
+#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=2423"
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define call_from_test
+#include "src/Geometry/!geometry_temp.hpp"
+#include "src/Geometry/min_enclosing_circle.hpp"
+#include "src/Graph/MatchingBipartite.hpp"
+#undef call_from_test
+
+signed main() {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  using namespace geometry;
+  int n, m;
+  cin >> n >> m;
+  MatchingBipartite graph(m, n);
+  Real rs[n];
+  for (int i = 0; i < n; i++) cin >> rs[i];
+  for (int j = 0; j < m; j++) {
+    int p;
+    cin >> p;
+    vector<Point> ps(p);
+    for (int k = 0; k < p; k++) cin >> ps[k];
+    Real r = min_enclosing_circle(ps).r;
+    for (int i = 0; i < n; i++)
+      if (sgn(rs[i] - r) >= 0) graph.add_edge(j, i);
+  }
+  auto ans = graph.lexicographically_matching();
+  if (ans.first < m)
+    cout << "NG" << endl;
+  else
+    for (int a : ans.second.first) cout << a + 1 << endl;
+  return 0;
+}

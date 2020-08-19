1db362657076af7a39b496bba6560ae2cbd46910
hashiryo <ryoma@hashimotoryoumanoMacBook-Pro.local>
2020-08-19 22:19:13 +0900

    modify

--- test/aoj/3049.test.cpp
+++ test/aoj/3049.test.cpp
@@ -1,6 +1,6 @@
 #define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=3049"
 
-#define ERROR "0.00000001"
+#define ERROR "0.000001"
 
 #include <bits/stdc++.h>
 using namespace std;

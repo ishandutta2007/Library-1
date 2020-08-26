62cc6acae7a838f814492c0e98149baafa89f927
hashiryo <ryoma@hashimotoryoumanoMacBook-Pro.local>
2020-08-26 21:11:40 +0900

    modify

--- src/Graph/StronglyConnectedComponents.hpp
+++ src/Graph/StronglyConnectedComponents.hpp
@@ -12,20 +12,17 @@ using namespace std;
 #endif
 
 struct StronglyConnectedComponents {
- private:
-  int n;
-  vector<vector<int>> radj;
+  vector<vector<int>> adj;
 
- public:
-  StronglyConnectedComponents(int n) : n(n), radj(n) {}
-  void add_edge(int src, int dst) { radj[dst].push_back(src); }
+  StronglyConnectedComponents(int n) : adj(n) {}
+  void add_edge(int src, int dst) { adj[src].push_back(dst); }
   pair<vector<vector<int>>, vector<int>> get_SCC() {
     vector<vector<int>> scc;
-    vector<int> S, B, index(n);
+    vector<int> S, B, index(adj.size());
     function<void(int)> dfs = [&](int u) {
       B.push_back(index[u] = S.size());
       S.push_back(u);
-      for (int v : radj[u]) {
+      for (int v : adj[u]) {
         if (!index[v])
           dfs(v);
         else
@@ -34,15 +31,17 @@ struct StronglyConnectedComponents {
       if (index[u] == B.back()) {
         scc.push_back({});
         B.pop_back();
-        for (; index[u] < S.size(); S.pop_back()) {
+        for (; index[u] < (int)S.size(); S.pop_back()) {
           scc.back().push_back(S.back());
-          index[S.back()] = n + scc.size();
+          index[S.back()] = adj.size() + scc.size();
         }
       }
     };
-    for (int u = 0; u < n; ++u)
+    for (size_t u = 0; u < adj.size(); u++)
       if (!index[u]) dfs(u);
-    for (int u = 0; u < n; ++u) index[u] -= n + 1;
+    reverse(scc.begin(), scc.end());
+    for (size_t u = 0; u < adj.size(); u++)
+      index[u] = scc.size() - index[u] + adj.size();
     return make_pair(scc, index);
   }
 };

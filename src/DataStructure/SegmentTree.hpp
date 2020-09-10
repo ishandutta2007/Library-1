c6a546263ca235227247e3019c40935c27b08e96
hashiryo <ryoma@hashimotoryoumanoMacBook-Pro.local>
2020-09-10 13:44:33 +0900

    modify

--- src/DataStructure/SegmentTree.hpp
+++ src/DataStructure/SegmentTree.hpp
@@ -1,8 +1,12 @@
 /**
  * @title Segment-Tree
  * @category データ構造
- * @brief O(logN)
+ * O(logN)
+ * 2冪にしていない(メモリ節約)
  */
+
+// 2冪にするのやめて二次元array(10x10)をのせることに成功
+// https://atcoder.jp/contests/arc027/tasks/arc027_4
 
 #ifndef call_from_test
 #include <bits/stdc++.h>
@@ -17,77 +21,82 @@ struct SegmentTree {
   const int n;
   vector<T> dat;
 
- private:
-  template <typename C>
-  int find_subtree(int a, const C &check, T &cur, bool type) {
-    while (a < n) {
-      T nxt
-          = type ? M::f(dat[2 * a + type], cur) : M::f(cur, dat[2 * a + type]);
-      if (check(nxt))
-        a = 2 * a + type;
-      else
-        cur = nxt, a = 2 * a + !type;
-    }
-    return a - n;
-  }
-
  public:
   SegmentTree() {}
-  SegmentTree(int n_, T v1 = M::ti()) : SegmentTree(vector<T>(n_, v1)) {}
-  SegmentTree(const vector<T> &v)
-      : n(1 << int(ceil(log2(v.size())))), dat(n << 1, M::ti()) {
+  SegmentTree(int n_) : n(n_), dat(n << 1, M::ti()) {}
+  SegmentTree(int n_, T v) : SegmentTree(vector<T>(n_, v)) {}
+  SegmentTree(const vector<T> &v) : n(v.size()), dat(n << 1, M::ti()) {
     for (int i = 0; i < (int)v.size(); i++) dat[i + n] = v[i];
-    for (int i = n - 1; i >= 1; i--)
-      dat[i] = M::f(dat[i << 1 | 0], dat[i << 1 | 1]);
+    rebuild();
   }
   void set_val(int k, T x) {
     for (dat[k += n] = x; k >>= 1;)
       dat[k] = M::f(dat[(k << 1) | 0], dat[(k << 1) | 1]);
   }
-  //[a,b)
-  T query(int a, int b) {
+  void unsafe_set(int k, T x) { dat[k + n] = x; }
+  void rebuild() {
+    for (int i = n - 1; i >= 1; i--)
+      dat[i] = M::f(dat[i << 1 | 0], dat[i << 1 | 1]);
+  }
+  void clear() { fill(dat.begin(), dat.end(), M::ti()); }
+  //[l,r)
+  inline T query(int l, int r) {
     T vl = M::ti(), vr = M::ti();
-    for (int l = a + n, r = b + n; l < r; l >>= 1, r >>= 1) {
-      if (l & 1) vl = M::f(vl, dat[l++]);
-      if (r & 1) vr = M::f(dat[--r], vr);
+    for (int a = l + n, b = r + n; a < b; a >>= 1, b >>= 1) {
+      if (a & 1) vl = M::f(vl, dat[a++]);
+      if (b & 1) vr = M::f(dat[--b], vr);
     }
     return M::f(vl, vr);
   }
   T operator[](const int &k) const { return dat[k + n]; }
-  // min { i : check(query(a,i+1)) = true }
-  template <typename C>
-  int find_first(const C &check, int a = 0) {
-    T vl = M::ti();
-    if (a <= 0) {
-      if (check(M::f(vl, dat[1]))) return find_subtree(1, check, vl, false);
-      return -1;
+
+  // max{ i : check(query(l,i+1)) = true}
+  template <class C>
+  int find_right(const C &check, int l = 0) {
+    assert(check(M::ti()));
+    if (l == n) return n;
+    vector<int> idl, idr;
+    for (int a = l + n, b = 2 * n; a < b; a >>= 1, b >>= 1) {
+      if (a & 1) idl.push_back(a++);
+      if (b & 1) idr.push_back(--b);
     }
-    int b = n;
-    for (a += n, b += n; a < b; a >>= 1, b >>= 1)
-      if (a & 1) {
-        T nxt = M::f(vl, dat[a]);
-        if (check(nxt)) return find_subtree(a, check, vl, false);
-        vl = nxt;
-        ++a;
+    for (auto itr = idr.rbegin(); itr != idr.rend(); itr++) idl.push_back(*itr);
+    T val = M::ti();
+    for (int i : idl) {
+      if (!check(M::f(val, dat[i]))) {
+        while (i < n) {
+          i = i << 1 | 0;
+          if (check(M::f(val, dat[i]))) val = M::f(val, dat[i++]);
+        }
+        return i - n;
       }
-    return -1;
+      val = M::f(val, dat[i]);
+    }
+    return n;
   }
-  // max { i : check(query(i,b)) = true }
-  template <typename C>
-  int find_last(const C &check, int b = -1) {
-    if (b < 0) b = n;
-    T vr = M::ti();
-    if (b >= n) {
-      if (check(M::f(dat[1], vr))) return find_subtree(1, check, vr, true);
-      return -1;
+  //   min { i : check(query(i,r)) = true }
+  template <class C>
+  int find_left(const C &check, int r = -1) {
+    if (r < 0) r = n;
+    assert(check(M::ti()));
+    if (r == 0) return 0;
+    vector<int> idl, idr;
+    for (int a = n, b = r + n; a < b; a >>= 1, b >>= 1) {
+      if (a & 1) idl.push_back(a++);
+      if (b & 1) idr.push_back(--b);
     }
-    int a = 0;
-    for (a += n, b += n; a < b; a >>= 1, b >>= 1)
-      if (b & 1) {
-        T nxt = M::f(dat[--b], vr);
-        if (check(nxt)) return find_subtree(b, check, vr, true);
-        vr = nxt;
+    for (auto itr = idl.rbegin(); itr != idl.rend(); itr++) idr.push_back(*itr);
+    T val = M::ti();
+    for (int i : idr) {
+      if (!check(M::f(dat[i], val))) {
+        while (i < n) {
+          i = i << 1 | 1;
+          if (check(M::f(dat[i], val))) val = M::f(dat[i--], val);
+        }
+        return i + 1 - n;
       }
-    return -1;
+      val = M::f(dat[i], val);
+    }
+    return 0;
   }
 };

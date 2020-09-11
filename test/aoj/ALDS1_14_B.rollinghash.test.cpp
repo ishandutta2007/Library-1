8ae74a1410e8b3db162bd36209bb454383831387
hashiryo <ryoma@hashimotoryoumanoMacBook-Pro.local>
2020-09-11 14:07:18 +0900

    modify

--- test/aoj/ALDS1_14_B.rollinghash.test.cpp
+++ test/aoj/ALDS1_14_B.rollinghash.test.cpp
@@ -13,16 +13,12 @@ signed main() {
   ios::sync_with_stdio(0);
   string T, P;
   cin >> T >> P;
-  using RH1 = RollingHash<2020031611>;
-  using RH2 = RollingHash<19990929>;
-  RH1 rt1(T), rp1(P);
-  RH2 rt2(T), rp2(P);
+  auto base1 = RollingHash::generate_base();
+  auto base2 = RollingHash::generate_base();
+  RollingHash_MultiBase rt(T, {base1, base2}), rp(P, {base1, base2});
   int N = P.length();
-  for (int i = 0; i + N <= T.length(); i++) {
-    if (rt1.get_hash(i, i + N) == rp1.get_hash(0, N)
-        && rt2.get_hash(i, i + N) == rp2.get_hash(0, N))
-      cout << i << "\n";
-  }
+  for (int i = 0; i + N <= T.length(); i++)
+    if (RollingHash_MultiBase::equal(rt, rp, i, i + N, 0, N)) cout << i << "\n";
   cout << flush;
   return 0;
 }

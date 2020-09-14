cf754dda0a8f8b7f300cedb17a5bc5a3cd8dcf53
hashiryo <ryoma@hashimotoryoumanoMacBook-Pro.local>
2020-09-14 13:17:44 +0900

    modify

--- test/aoj/ALDS1_14_B.rollinghash.test.cpp
+++ test/aoj/ALDS1_14_B.rollinghash.test.cpp
@@ -11,14 +11,15 @@ using namespace std;
 signed main() {
   cin.tie(0);
   ios::sync_with_stdio(0);
+  using RHMB = RollingHash_MultiBase<2>;
   string T, P;
   cin >> T >> P;
   auto base1 = RollingHash::generate_base();
   auto base2 = RollingHash::generate_base();
-  RollingHash_MultiBase rt(T, {base1, base2}), rp(P, {base1, base2});
+  RHMB rt(T, {base1, base2}), rp(P, {base1, base2});
   int N = P.length();
   for (int i = 0; i + N <= T.length(); i++)
-    if (RollingHash_MultiBase::equal(rt, rp, i, i + N, 0, N)) cout << i << "\n";
+    if (RHMB::equal(rt, rp, i, i + N, 0, N)) cout << i << "\n";
   cout << flush;
   return 0;
 }

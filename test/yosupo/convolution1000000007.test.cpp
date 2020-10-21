666dbe23bf3ba0428ca690d2e2c4a0b2c977747b
hashiryo <ryoma@hashimotoryoumanoMacBook-Pro.local>
2020-10-21 17:49:35 +0900

    modify

--- test/yosupo/convolution1000000007.test.cpp
+++ test/yosupo/convolution1000000007.test.cpp
@@ -11,14 +11,15 @@ using namespace std;
 signed main() {
   cin.tie(0);
   ios::sync_with_stdio(0);
-  int N, M;
-  cin >> N >> M;
   using Mint = ModInt<int(1e9 + 7)>;
   using FPS = FormalPowerSeries<Mint>;
+  int N, M;
+  cin >> N >> M;
   FPS a(N), b(M);
   for (int i = 0; i < N; i++) cin >> a[i];
   for (int j = 0; j < M; j++) cin >> b[j];
   auto c = a * b;
+  c.resize(N + M - 1);
   for (int k = 0; k < N + M - 1; k++) {
     cout << c[k] << (k < N + M - 2 ? " " : "\n");
   }

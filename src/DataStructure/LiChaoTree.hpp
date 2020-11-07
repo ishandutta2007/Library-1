8dfa232380682879566af978d3366254cd286fc9
hashiryo <ryoma@hashimotoryoumanoMacBook-Pro.local>
2020-11-07 14:11:46 +0900

    modify lichao

--- src/DataStructure/LiChaoTree.hpp
+++ src/DataStructure/LiChaoTree.hpp
@@ -3,16 +3,16 @@
 /**
  * @title Li-Chao-Tree
  * @category データ構造
- * @brief 直線挿入:O(n log n)
- * @brief 線分挿入:O(n log^2 n)
- * @brief 最小値取得:O(n log n)
- * @brief 最大値取得したいなら(-a,-b)を挿入して-最小値を取得
+ * 直線挿入:O(n log n)
+ * 線分挿入:O(n log^2 n)
+ * 最小値取得:O(n log n)
+ * 最大値取得したいなら(-a,-b)を挿入して-最小値を取得
  */
 
 // BEGIN CUT HERE
 
-template <typename T, T x_low = -(1 << 30), T x_high = (1 << 30),
-          size_t LIM = (1 << 23)>
+template <typename T, T lower = -(1 << 30), T upper = (1 << 30),
+          std::size_t LIM = (1 << 23)>
 struct LiChaoTree {
   struct Line {
     T a, b;
@@ -20,10 +20,10 @@ struct LiChaoTree {
     inline T get(T x) const { return a * x + b; }
   };
   struct Node {
-    Line x;
+    Line f;
     Node *ch[2];
-    Node() : x(0, INF) {}
-    Node(const Line &x) : x(x), ch{nullptr, nullptr} {}
+    Node() : f(0, INF) {}
+    Node(const Line &f_) : f(f_), ch{nullptr, nullptr} {}
     void *operator new(size_t) {
       static std::vector<Node> pool(LIM);
       return &pool[node_count++];
@@ -32,67 +32,41 @@ struct LiChaoTree {
 
  private:
   static constexpr T INF = std::numeric_limits<T>::max() / 2;
-  static int node_count;
+  static inline int node_count;
   Node *root;
 
  private:
-  Node *insert_line(Node *t, Line &x, const T &l, const T &r, const T &x_l,
-                    const T &x_r) {
-    if (!t) return new Node(x);
-    T t_l = t->x.get(l), t_r = t->x.get(r);
-    if (t_l <= x_l && t_r <= x_r) {
-      return t;
-    } else if (t_l >= x_l && t_r >= x_r) {
-      t->x = x;
-      return t;
-    } else {
-      T m = (l + r) / 2;
-      if (m == r) --m;
-      T t_m = t->x.get(m), x_m = x.get(m);
-      if (t_m > x_m) {
-        std::swap(t->x, x);
-        if (x_l >= t_l)
-          t->ch[0] = insert_line(t->ch[0], x, l, m, t_l, t_m);
-        else
-          t->ch[1] = insert_line(t->ch[1], x, m + 1, r, t_m + x.a, t_r);
-      } else {
-        if (t_l >= x_l)
-          t->ch[0] = insert_line(t->ch[0], x, l, m, x_l, x_m);
-        else
-          t->ch[1] = insert_line(t->ch[1], x, m + 1, r, x_m + x.a, x_r);
-      }
-      return t;
-    }
-  }
-  Node *insert_segment(Node *t, Line &x, const T &a, const T &b, const T &l,
-                       const T &r, const T &x_l, const T &x_r) {
-    if (r < a || b < l) return t;
-    if (a <= l && r <= b) {
-      Line y{x};
-      return insert_line(t, y, l, r, x_l, x_r);
-    }
-    if (t) {
-      T t_l = t->x.get(l), t_r = t->x.get(r);
-      if (t_l <= x_l && t_r <= x_r) return t;
-    } else {
-      t = new Node(Line(0, INF));
-    }
-    T m = (l + r) / 2;
-    if (m == r) --m;
-    T x_m = x.get(m);
-    t->ch[0] = insert_segment(t->ch[0], x, a, b, l, m, x_l, x_m);
-    t->ch[1] = insert_segment(t->ch[1], x, a, b, m + 1, r, x_m + x.a, x_r);
+  Node *insert_line(Node *t, Line f, const T &x_l, const T &x_r) {
+    if (!t) return new Node(f);
+    T y_l = f.get(x_l), y_r = f.get(x_r), ty_l = t->f.get(x_l),
+      ty_r = t->f.get(x_r);
+    if (ty_l <= y_l && ty_r <= y_r) return t;
+    if (ty_l >= y_l && ty_r >= y_r) return t->f = f, t;
+    T x_m = x_l == x_r - 1 ? x_l : (x_l + x_r) / 2, ty_m = t->f.get(x_m),
+      y_m = f.get(x_m);
+    if (ty_m > y_m) std::swap(t->f, f), std::swap(y_l, ty_l);
+    if (ty_l >= y_l) t->ch[0] = insert_line(t->ch[0], f, x_l, x_m);
+    if (ty_l < y_l) t->ch[1] = insert_line(t->ch[1], f, x_m + 1, x_r);
     return t;
   }
-  T query(const Node *t, const T &l, const T &r, const T &x) const {
+  Node *insert_segment(Node *t, const Line &f, const T &l, const T &r,
+                       const T &x_l, const T &x_r) {
+    if (x_r < l || r < x_l) return t;
+    if (l <= x_l && x_r <= r) return insert_line(t, f, x_l, x_r);
+    T y_l = f.get(x_l), y_r = f.get(x_r);
+    if (t && t->f.get(x_l) <= y_l && t->f.get(x_r) <= y_r) return t;
+    if (!t) t = new Node(Line(0, INF));
+    T x_m = x_l == x_r - 1 ? x_l : (x_l + x_r) / 2;
+    t->ch[0] = insert_segment(t->ch[0], f, l, r, x_l, x_m);
+    t->ch[1] = insert_segment(t->ch[1], f, l, r, x_m + 1, x_r);
+    return t;
+  }
+  T query(const Node *t, const T &x_l, const T &x_r, const T &x) const {
     if (!t) return INF;
-    if (l == r) return t->x.get(x);
-    T m = (l + r) / 2;
-    if (m == r) --m;
-    if (x <= m)
-      return std::min(t->x.get(x), query(t->ch[0], l, m, x));
-    else
-      return std::min(t->x.get(x), query(t->ch[1], m + 1, r, x));
+    if (x_l == x_r) return t->f.get(x);
+    T x_m = x_l == x_r - 1 ? x_l : (x_l + x_r) / 2;
+    return std::min(t->f.get(x), x <= x_m ? query(t->ch[0], x_l, x_m, x)
+                                          : query(t->ch[1], x_m + 1, x_r, x));
   }
 
  public:
@@ -102,17 +76,13 @@ struct LiChaoTree {
     node_count = 0;
     root = nullptr;
   }
+  // ax+b
   void insert_line(T a, T b) {
-    Line x(a, b);
-    root = insert_line(root, x, x_low, x_high, x.get(x_low), x.get(x_high));
+    root = insert_line(root, Line(a, b), lower, upper);
   }
-  // [l,r)
+  // ax+b for x in [l,r)
   void insert_segment(T l, T r, T a, T b) {
-    Line x(a, b);
-    root = insert_segment(root, x, l, r - 1, x_low, x_high, x.get(x_low),
-                          x.get(x_high));
+    root = insert_segment(root, Line(a, b), l, r - 1, lower, upper);
   }
-  T query(T x) const { return query(root, x_low, x_high, x); }
+  T query(T x) const { return query(root, lower, upper, x); }
 };
-template <typename T, T x_low, T x_high, size_t LIM>
-int LiChaoTree<T, x_low, x_high, LIM>::node_count = 0;

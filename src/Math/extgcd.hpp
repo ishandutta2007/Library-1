c22fba1b4ec715d020973993c1da399e152ad69b
hashiryo <ryoma@hashimotoryoumanoMacBook-Pro.local>
2021-02-13 14:05:50 +0900

    modify

--- src/Math/extgcd.hpp
+++ src/Math/extgcd.hpp
@@ -48,35 +48,36 @@ FormalPowerSeries<mint> extgcd(FormalPowerSeries<mint> a,
     return pm{pv{lhs[0][1], lhs[0][0] - lhs[0][1] * q},
               pv{lhs[1][1], lhs[1][0] - lhs[1][1] * q}};
   };
-  std::function<pm(poly, poly)> hgcd = [&](const poly &p0, const poly &p1) {
+  auto hgcd = [&](auto rec, const poly &p0, const poly &p1) -> pm {
     assert(p0.deg() > p1.deg());
     int m = ((p0.deg() - 1) >> 1) + 1, n = p1.deg();
     if (n < m) return pm{pv{poly{1}, poly{}}, pv{poly{}, poly{1}}};
-    pm R(hgcd(poly(p0.begin() + m, p0.end()), poly(p1.begin() + m, p1.end())));
+    pm R(rec(rec, poly(p0.begin() + m, p0.end()),
+             poly(p1.begin() + m, p1.end())));
     pv ab(mulv(R, pv{p0, p1}));
     if (ab[1].deg() < m) return R;
     std::pair<poly, poly> qr(ab[0].quorem(ab[1]));
     int k = 2 * m - ab[1].deg();
     if ((int)qr.second.size() <= k) return mulQ_l(qr.first, R);
-    return mul(hgcd(poly(ab[1].begin() + k, ab[1].end()),
-                    poly(qr.second.begin() + k, qr.second.end())),
+    return mul(rec(rec, poly(ab[1].begin() + k, ab[1].end()),
+                   poly(qr.second.begin() + k, qr.second.end())),
                mulQ_l(qr.first, R));
   };
-  std::function<pm(poly, poly)> cogcd = [&](const poly &p0, const poly &p1) {
+  auto cogcd = [&](auto rec, const poly &p0, const poly &p1) -> pm {
     assert(p0.deg() > p1.deg());
-    pm M(hgcd(p0, p1));
+    pm M(hgcd(hgcd, p0, p1));
     pv p2p3(mulv(M, pv{p0, p1}));
     if (p2p3[1].deg() == -1) return M;
     std::pair<poly, poly> qr(p2p3[0].quorem(p2p3[1]));
     if (qr.second.deg() == -1) return mulQ_l(qr.first, M);
-    return mul(cogcd(p2p3[1], qr.second), mulQ_l(qr.first, M));
+    return mul(rec(rec, p2p3[1], qr.second), mulQ_l(qr.first, M));
   };
   pm c;
   if (a.norm().deg() > b.norm().deg()) {
-    c = cogcd(a, b);
+    c = cogcd(cogcd, a, b);
   } else {
     std::pair<poly, poly> qr(a.quorem(b));
-    c = mulQ_r(cogcd(b, qr.second), qr.first);
+    c = mulQ_r(cogcd(cogcd, b, qr.second), qr.first);
   }
   return a * (x = c[0][0]) + b * (y = c[0][1]);
 }

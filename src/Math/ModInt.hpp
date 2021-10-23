23178a5af575cbd7cd5724833053c869f0ace7d5
hashiryo <hashimotoryoma@gmail.com>
2021-10-23 18:23:39 +0900

    add LSI test

--- src/Math/ModInt.hpp
+++ src/Math/ModInt.hpp
@@ -7,39 +7,11 @@
 
 // BEGIN CUT HERE
 
-template <std::uint64_t mod, std::uint64_t prim_root = 0>
-class ModInt {
-  using u64 = std::uint64_t;
-  using u128 = __uint128_t;
-  static constexpr u64 mul_inv(u64 n, int e = 6, u64 x = 1) {
-    return e == 0 ? x : mul_inv(n, e - 1, x * (2 - x * n));
-  }
-  static constexpr u64 inv = mul_inv(mod, 6, 1), r2 = -u128(mod) % mod;
-  static constexpr u64 init(u64 w) { return reduce(u128(w) * r2); }
-  static constexpr u64 reduce(const u128 w) {
-    return u64(w >> 64) + mod - ((u128(u64(w) * inv) * mod) >> 64);
-  }
-
- public:
-  constexpr ModInt() : x(0) {}
-  constexpr ModInt(std::int64_t n) : x(init(n < 0 ? mod - (-n) % mod : n)) {}
-  ~ModInt() = default;
-  static constexpr u64 modulo() { return mod; }
-  static constexpr u64 norm(u64 w) { return w - (mod & -(w >= mod)); }
-  static constexpr u64 pr_rt() { return prim_root; }
-  constexpr ModInt operator-() const {
-    ModInt ret;
-    return ret.x = ((mod << 1) & -(x != 0)) - x, ret;
-  }
-  constexpr ModInt &operator+=(const ModInt &rhs) {
-    return x += rhs.x - (mod << 1), x += (mod << 1) & -(x >> 63), *this;
-  }
-  constexpr ModInt &operator-=(const ModInt &rhs) {
-    return x -= rhs.x, x += (mod << 1) & -(x >> 63), *this;
-  }
-  constexpr ModInt &operator*=(const ModInt &rhs) {
-    return this->x = reduce(u128(this->x) * rhs.x), *this;
-  }
+namespace internal {
+template <std::uint64_t mod, std::uint64_t prim_root, class ModInt>
+class ModIntImpl {
+  static constexpr std::uint64_t modulo() { return mod; }
+  static constexpr std::uint64_t pr_rt() { return prim_root; }
   constexpr ModInt &operator/=(const ModInt &rhs) {
     return this->operator*=(rhs.inverse());
   }
@@ -47,13 +19,7 @@ class ModInt {
   ModInt operator-(const ModInt &rhs) const { return ModInt(*this) -= rhs; }
   ModInt operator*(const ModInt &rhs) const { return ModInt(*this) *= rhs; }
   ModInt operator/(const ModInt &rhs) const { return ModInt(*this) /= rhs; }
-  bool operator==(const ModInt &rhs) const { return norm(x) == norm(rhs.x); }
-  bool operator!=(const ModInt &rhs) const { return norm(x) != norm(rhs.x); }
-  u64 val() const {
-    u64 ret = reduce(x) - mod;
-    return ret + (mod & -(ret >> 63));
-  }
-  constexpr ModInt pow(u64 k) const {
+  constexpr ModInt pow(std::uint64_t k) const {
     ModInt ret = ModInt(1);
     for (ModInt base = *this; k; k >>= 1, base *= base)
       if (k & 1) ret *= base;
@@ -70,17 +36,76 @@ class ModInt {
       ModInt b = (u.first * v.second + u.second * v.first);
       return std::make_pair(a, b);
     };
-    u64 e = (mod + 1) >> 1;
+    std::uint64_t e = (mod + 1) >> 1;
     auto ret = std::make_pair(ONE, ModInt(0));
     for (auto bs = std::make_pair(b, ONE); e; e >>= 1, bs = mul(bs, bs))
       if (e & 1) ret = mul(ret, bs);
     return ret.first.val() * 2 < mod ? ret.first : -ret.first;
   }
-  friend std::istream &operator>>(std::istream &is, ModInt &rhs) {
-    return is >> rhs.x, rhs.x = init(rhs.x), is;
-  }
   friend std::ostream &operator<<(std::ostream &os, const ModInt &rhs) {
     return os << rhs.val();
   }
+};
+}  // namespace internal
+template <std::uint64_t mod, std::uint64_t prim_root = 0>
+class ModInt : internal::ModIntImpl<mod, prim_root, ModInt<mod, prim_root>> {
+  using u64 = std::uint64_t;
+  using u128 = __uint128_t;
+  static constexpr u64 mul_inv(u64 n, int e = 6, u64 x = 1) {
+    return e == 0 ? x : mul_inv(n, e - 1, x * (2 - x * n));
+  }
+  static constexpr u64 inv = mul_inv(mod, 6, 1), r2 = -u128(mod) % mod;
+  static constexpr u64 init(u64 w) { return reduce(u128(w) * r2); }
+  static constexpr u64 reduce(const u128 w) {
+    return u64(w >> 64) + mod - ((u128(u64(w) * inv) * mod) >> 64);
+  }
   u64 x;
+
+ public:
+  constexpr ModInt() : x(0) {}
+  constexpr ModInt(std::int64_t n) : x(init(n < 0 ? mod - (-n) % mod : n)) {}
+  ~ModInt() = default;
+  static constexpr u64 norm(u64 w) { return w - (mod & -(w >= mod)); }
+  constexpr ModInt operator-() const {
+    ModInt ret;
+    return ret.x = ((mod << 1) & -(x != 0)) - x, ret;
+  }
+  constexpr ModInt &operator+=(const ModInt &rhs) {
+    return x += rhs.x - (mod << 1), x += (mod << 1) & -(x >> 63), *this;
+  }
+  constexpr ModInt &operator-=(const ModInt &rhs) {
+    return x -= rhs.x, x += (mod << 1) & -(x >> 63), *this;
+  }
+  constexpr ModInt &operator*=(const ModInt &rhs) {
+    return this->x = reduce(u128(this->x) * rhs.x), *this;
+  }
+  bool operator==(const ModInt &rhs) const { return norm(x) == norm(rhs.x); }
+  bool operator!=(const ModInt &rhs) const { return norm(x) != norm(rhs.x); }
+  u64 val() const {
+    u64 ret = reduce(x) - mod;
+    return ret + (mod & -(ret >> 63));
+  }
+  friend std::istream &operator>>(std::istream &is, ModInt &rhs) {
+    return is >> rhs.x, rhs.x = init(rhs.x), is;
+  }
+};
+template <std::uint64_t prim_root>
+class ModInt<2, prim_root>
+    : internal::ModIntImpl<2, prim_root, ModInt<2, prim_root>> {
+  bool x;
+
+ public:
+  constexpr ModInt() : x(0) {}
+  constexpr ModInt(std::int64_t n) : x(n & 1) {}
+  ~ModInt() = default;
+  constexpr ModInt operator-() const { return *this; }
+  constexpr ModInt &operator+=(const ModInt &rhs) { return x ^= rhs.x, *this; }
+  constexpr ModInt &operator-=(const ModInt &rhs) { return x ^= rhs.x, *this; }
+  constexpr ModInt &operator*=(const ModInt &rhs) { return x &= rhs.x, *this; }
+  bool operator==(const ModInt &rhs) const { return x == rhs.x; }
+  bool operator!=(const ModInt &rhs) const { return x != rhs.x; }
+  std::uint64_t val() const { return x; }
+  friend std::istream &operator>>(std::istream &is, ModInt &rhs) {
+    return is >> rhs.x, is;
+  }
 };
---
+++ test/atcoder/cf17_final_e.test.cpp
@@ -0,0 +1,32 @@
+#define PROBLEM "https://atcoder.jp/contests/cf17-final/tasks/cf17_final_e"
+#include <bits/stdc++.h>
+#include "Math/ModInt.hpp"
+#include "Graph/LinearSystemIncidence.hpp"
+using namespace std;
+
+signed main() {
+  cin.tie(0);
+  ios::sync_with_stdio(0);
+  using Mint = ModInt<26>;
+  string S;
+  cin >> S;
+  int N, M = S.length() + 1;
+  cin >> N;
+  LinearSystemIncidence<Mint> lsi((M + 1) / 2);
+  for (int i = 0; i < N; i++) {
+    int L, R;
+    cin >> L >> R;
+    L--;
+    if (2 * L >= M) L = M - 1 - L;
+    if (2 * R >= M) R = M - 1 - R;
+    lsi.add_edge(L, R);
+  }
+  vector<Mint> b((M + 1) / 2);
+  b[0] = S[0] - S.back();
+  for (int i = 1; i < M / 2; i++)
+    b[i] = (S[i] - S[i - 1]) + (S[M - 1 - i] - S[M - 2 - i]);
+  if (M & 1) b[M / 2] = S[M / 2] - S[M / 2 - 1];
+  auto [isok, sol] = lsi.solve(b);
+  cout << (isok ? "YES" : "NO") << '\n';
+  return 0;
+}

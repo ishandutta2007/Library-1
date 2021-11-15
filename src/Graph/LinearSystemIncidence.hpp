ae4145d0ae78630e0bc048ccbb50e105dc5c6c67
hashiryo <hashimotoryoma@gmail.com>
2021-11-15 18:56:51 +0900

    modify LSI

--- src/Graph/LinearSystemIncidence.hpp
+++ src/Graph/LinearSystemIncidence.hpp
@@ -20,7 +20,7 @@ class LinearSystemIncidence {
   std::vector<T> x;
   std::vector<char> used;
   std::vector<std::vector<std::tuple<int, int, bool>>> adj;
-  T dfs(std::vector<T> &b, int u) {
+  T dfs(const std::vector<T> &b, int u) {
     used[u] = true;
     T ret = b[u];
     for (auto [id, to, fwd] : adj[u])
@@ -39,11 +39,11 @@ class LinearSystemIncidence {
   void add_edge(int src, int dst) {
     adj[src].emplace_back(m, dst, true), adj[dst].emplace_back(m++, src, false);
   }
-  std::pair<bool, std::vector<T>> solve(std::vector<T> &b) {
+  std::vector<T> solve(const std::vector<T> &b) {
     x.assign(m, T(0)), used.assign(adj.size(), false);
     for (std::size_t u = 0; u < adj.size(); u++)
       if (!used[u] && dfs(b, u) != T(0))
-        return {false, std::vector<T>()};  // no sloution
-    return {true, std::move(x)};
+        return std::vector<T>();  // no sloution
+    return std::move(x);
   }
 };

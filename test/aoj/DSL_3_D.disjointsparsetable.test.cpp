06b087a259d7bab81faafc33d3cffec5213a3fe4
hashiryo <hashimotoryoma@gmail.com>
2021-11-21 22:51:09 +0900

    modify sparsetable

--- test/aoj/DSL_3_D.disjointsparsetable.test.cpp
+++ test/aoj/DSL_3_D.disjointsparsetable.test.cpp
@@ -2,8 +2,8 @@
   "https://onlinejudge.u-aizu.ac.jp/courses/library/3/DSL/3/DSL_3_D"
 #include <bits/stdc++.h>
 #include "src/DataStructure/DisjointSparseTable.hpp"
-using namespace std;
 
+using namespace std;
 signed main() {
   cin.tie(0);
   ios::sync_with_stdio(0);
@@ -11,12 +11,8 @@ signed main() {
   cin >> N >> L;
   vector<int> a(N);
   for (int i = 0; i < N; i++) cin >> a[i];
-  auto f = [](int a, int b) { return min(a, b); };
-  DisjointSparseTable<int> dst(a, f);
-  for (int i = 0; i + L <= N; i++) {
-    if (i) cout << " ";
-    cout << dst.query(i, i + L);
-  }
-  cout << endl;
+  DisjointSparseTable<int> dst(a, [](int a, int b) { return min(a, b); });
+  for (int i = 0; i + L <= N; i++) cout << (i ? " " : "") << dst.fold(i, i + L);
+  cout << '\n';
   return 0;
 }

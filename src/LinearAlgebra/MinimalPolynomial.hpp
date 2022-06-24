bfab6ec5fc70dd7393f06e37c9ffcc0d2feb265e
hashiryo <hashimotoryoma@gmail.com>
2022-06-24 13:34:24 +0900

    modify minpoly

--- src/LinearAlgebra/MinimalPolynomial.hpp
+++ src/LinearAlgebra/MinimalPolynomial.hpp
@@ -17,15 +17,17 @@ class MinimalPolynomial {
   static const inline mod_t ZERO = 0;
   std::vector<mod_t> poly, rev;
   std::vector<Vec> bs;
-  int dg, n;
-  std::vector<mod_t> bostan_mori_msb(const std::vector<mod_t> &q,
-                                     std::uint64_t k) {
-    int d = q.size() - 1;
-    for (;; d--)
-      if (d < 0 || q[d] != ZERO) break;
+  std::size_t dg, n;
+  static inline int deg(const std::vector<mod_t> &p) {
+    for (int d = p.size() - 1;; d--)
+      if (d < 0 || p[d] != ZERO) return d;
+  }
+  static inline std::vector<mod_t> bostan_mori_msb(const std::vector<mod_t> &q,
+                                                   std::uint64_t k) {
+    int d = deg(q);
     assert(d >= 0), assert(q[0] != ZERO);
     std::vector<mod_t> ret(std::max(d, 1));
-    if (k == 0) return ret.back() = mod_t(1) / q[0], ret;
+    if (k == 0) return ret.back() = mod_t(1), ret;
     std::vector<mod_t> v(d + 1);
     for (int i = 0; i <= d; i += 2)
       for (int j = 0; j <= d; j += 2) v[(i + j) >> 1] += q[i] * q[j];
@@ -38,10 +40,11 @@ class MinimalPolynomial {
       for (int j = 1; j <= d; j += 2) ret[i - d] -= q[j] * w[(i - j) >> 1];
     return ret;
   }
-  std::vector<mod_t> x_pow_mod(std::uint64_t k) {
-    std::vector<mod_t> ret(dg), u = bostan_mori_msb(rev, k);
+  std::vector<mod_t> x_pow_mod(std::uint64_t k) const {
+    assert(k >= n);
+    std::vector<mod_t> ret(n), u = bostan_mori_msb(rev, k - n + dg);
     for (int i = dg; i--;)
-      for (int j = i + 1; j--;) ret[dg - 1 - i] += u[j] * rev[i - j];
+      for (int j = i + 1; j--;) ret[n - 1 - i] += u[j] * rev[i - j];
     return ret;
   }
 
@@ -57,17 +60,18 @@ class MinimalPolynomial {
       for (tmp = 0, j = n; j--;) tmp += a[j] * b[j];
       if (i) b = M * b;
     }
-    rev = berlekamp_massey(v), dg = rev.size();
+    rev = berlekamp_massey(v);
     for (auto &x : rev) x = -x;
     rev.insert(rev.begin(), 1), poly = rev;
+    rev.erase(rev.begin() + (dg = deg(rev)) + 1, rev.end());
     std::reverse(poly.begin(), poly.end());
-    bs.erase(bs.begin() + dg + 1, bs.end());
   }
   static std::uint64_t get_rand(std::uint64_t l, std::uint64_t r) {
     static std::mt19937_64 gen(std::random_device{}());
     return std::uniform_int_distribution<std::uint64_t>(l, r)(gen);
   }
-  Vec pow(std::uint64_t k) {  // M^k * b
+  Vec pow(std::uint64_t k) const {  // M^k * b
+    if (k < n) return bs[k];
     auto r = x_pow_mod(k);
     Vec ret = bs[0];
     for (auto &x : ret) x *= r[0];

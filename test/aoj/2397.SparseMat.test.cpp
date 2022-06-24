15dfc5747c3721f22a145189944ed6803ae5a62e
hashiryo <hashimotoryoma@gmail.com>
2022-06-24 14:06:30 +0900

    add minmaxrig

--- test/aoj/2397.SparseMat.test.cpp
+++ test/aoj/2397.SparseMat.test.cpp
@@ -1,4 +1,5 @@
-#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=2397"
+#define PROBLEM \
+  "https://onlinejudge.u-aizu.ac.jp/challenges/sources/JAG/Spring/2397"
 
 #include <bits/stdc++.h>
 #include "src/LinearAlgebra/SparseSquareMatrix.hpp"

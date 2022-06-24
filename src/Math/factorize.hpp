299c083134c58e1f5cbbb6a4badf908308e3768e
hashiryo <hashimotoryoma@gmail.com>
2022-06-24 14:27:36 +0900

    update factorize

--- src/Math/factorize.hpp
+++ src/Math/factorize.hpp
@@ -1,70 +1,117 @@
 #pragma once
 #include <bits/stdc++.h>
 /**
- * @title 高速素因数分解
+ * @title 高速素因数分解など
  * @category 数学
  *  O(N^(1/4))
- * ついでに原始根探索
+ * constexpr で呼べる
  */
 
 // BEGIN CUT HERE
 
-bool is_prime(std::uint64_t n) {
-  if (n < 2 || n % 6 % 4 != 1) return (n | 1) == 3;
-  __uint128_t s = __builtin_ctzll(n - 1), d = n >> s, p, i, e, b;
-  for (auto a : {2, 325, 9375, 28178, 450775, 9780504, 1795265022}) {
-    for (p = 1, i = s, e = d, b = a % n; e; e >>= 1, b = b * b % n)
-      if (e & 1) p = p * b % n;
-    while (p != 1 && p != n - 1 && a % n && i--) p = p * p % n;
+#ifndef MATH_TEMP
+#define MATH_TEMP
+constexpr std::uint16_t bsf(std::uint64_t n) {
+  constexpr std::uint8_t convert[64] = {
+      0,  1,  2,  53, 3,  7,  54, 27, 4,  38, 41, 8,  34, 55, 48, 28,
+      62, 5,  39, 46, 44, 42, 22, 9,  24, 35, 59, 56, 49, 18, 29, 11,
+      63, 52, 6,  26, 37, 40, 33, 47, 61, 45, 43, 21, 23, 58, 17, 10,
+      51, 25, 36, 32, 60, 20, 57, 16, 50, 31, 19, 15, 30, 14, 13, 12};
+  return convert[(n & ~(n - 1)) * 157587932685088877 >> 58];
+}
+constexpr std::uint64_t mul(std::uint64_t x, std::uint64_t y, std::uint64_t m) {
+  return (__uint128_t)x * y % m;
+}
+constexpr std::uint64_t pow(std::uint64_t x, std::uint64_t k, std::uint64_t m) {
+  for (std::uint64_t ret = 1;; x = mul(x, x, m))
+    if (k & 1 ? ret = mul(ret, x, m) : 0; !(k >>= 1)) return ret;
+}
+template <std::uint64_t... args>
+constexpr bool miller_rabin(std::uint64_t n) {
+  const std::uint64_t s = bsf(n - 1), d = n >> s;
+  for (auto a : {args...}) {
+    std::uint64_t b = a % n, p = pow(b, d, n), i = s;
+    while (p != 1 && p != n - 1 && b && i--) p = mul(p, p, n);
     if (p != n - 1 && i != s) return false;
   }
   return true;
 }
-
-std::vector<std::uint64_t> factorize(std::uint64_t n) {
-  std::vector<std::uint64_t> ret;
-  std::queue<std::uint64_t> que;
-  for (que.push(n); !que.empty();) {
-    __uint128_t m = que.front(), x = 0, y = 0, t = 0, prd = 2, i = 1, q;
-    que.pop();
-    if (m == 1) continue;
-    if (!is_prime(m)) {
-      auto f = [&](__uint128_t a) { return a * a % m + 1; };
-      while (t++ % 40 || std::gcd<std::uint64_t, std::uint64_t>(prd, m) == 1) {
-        if (x == y) x = ++i, y = f(x);
-        if ((q = prd * (std::max(x, y) - std::min(x, y)) % m)) prd = q;
-        x = f(x), y = f(f(y));
-      }
-      auto a = std::gcd<std::uint64_t, std::uint64_t>(prd, m);
-      que.push(a), que.push(m / a);
-    } else
-      ret.push_back(m);
-  }
-  return std::sort(ret.begin(), ret.end()), ret;
+constexpr bool is_prime(std::uint64_t n) {
+  if (n < 2 || n % 6 % 4 != 1) return (n | 1) == 3;
+  if (n < UINT_MAX) return miller_rabin<2, 7, 61>(n);
+  return miller_rabin<2, 325, 9375, 28178, 450775, 9780504, 1795265022>(n);
+}
+template <class T>
+constexpr void bubble_sort(T *bg, T *ed) {
+  for (int sz = ed - bg, i = 0; i < sz; i++)
+    for (int j = sz; --j > i;)
+      if (auto tmp = bg[j - 1]; bg[j - 1] > bg[j])
+        bg[j - 1] = bg[j], bg[j] = tmp;
 }
 
-std::map<std::uint64_t, short> factorize_map(std::uint64_t n) {
-  std::map<std::uint64_t, short> ret;
-  for (auto &p : factorize(n)) ret[p]++;
+template <class T, std::size_t _Nm>
+struct ConstexprArray {
+  constexpr std::size_t size() const { return _sz; }
+  constexpr auto &operator[](int i) const { return dat[i]; }
+  constexpr auto *begin() const { return dat; }
+  constexpr auto *end() const { return dat + _sz; }
+
+ protected:
+  T dat[_Nm] = {};
+  std::size_t _sz = 0;
+};
+class Factors
+    : public ConstexprArray<std::pair<std::uint64_t, std::uint16_t>, 16> {
+  static constexpr std::uint64_t rho(std::uint64_t n, std::uint64_t c) {
+    auto f = [n, c](std::uint64_t x) { return ((__uint128_t)x * x + c) % n; };
+    std::uint64_t x = 1, y = 2, z = 1, q = 1, g = 1;
+    const std::uint64_t m = 1LL << (std::__lg(n) / 5);
+    for (std::uint64_t r = 1, i = 0; g == 1; r <<= 1) {
+      for (x = y, i = r; i--;) y = f(y);
+      for (std::uint64_t k = 0; k < r && g == 1; g = std::gcd(q, n), k += m)
+        for (z = y, i = std::min(m, r - k); i--;)
+          y = f(y), q = mul(q, (x > y ? x - y : y - x), n);
+    }
+    if (g == n) do {
+        z = f(z), g = std::gcd((x > z ? x - z : z - x), n);
+      } while (g == 1);
+    return g;
+  }
+  static constexpr std::uint64_t find_prime_factor(std::uint64_t n) {
+    if (is_prime(n)) return n;
+    for (std::uint64_t i = 100, m = 0; i--; n = m)
+      if (m = rho(n, i + 1); is_prime(m)) return m;
+    return 0;
+  }
+  constexpr void init(std::uint64_t n) {
+    for (std::uint64_t p = 2; p < 100 && p * p <= n; p++)
+      if (n % p == 0) {
+        for (; n % p == 0;) n /= p, dat[_sz].second++;
+        dat[_sz++].first = p;
+      }
+    for (std::uint64_t p = 0; n > 1; dat[_sz++].first = p)
+      for (p = find_prime_factor(n); n % p == 0;) n /= p, dat[_sz].second++;
+    bubble_sort(dat, dat + _sz);
+  }
+
+ public:
+  constexpr Factors() = default;
+  constexpr Factors(std::uint64_t n) { init(n); }
+};
+constexpr std::uint64_t totient(const Factors &f) {
+  std::uint64_t ret = 1, i = 0;
+  for (const auto &[p, e] : f)
+    for (ret *= p - 1, i = e; --i;) ret *= p;
   return ret;
 }
-
-std::vector<std::uint64_t> divisors(std::uint64_t n) {
-  std::vector<std::uint64_t> ret = {1};
-  for (auto [p, e] : factorize_map(n))
-    for (std::uint64_t sz = ret.size(), pw = p; e--; pw *= p)
-      for (int i = sz - 1; i >= 0; i--) ret.push_back(ret[i] * pw);
-  return std::sort(ret.begin(), ret.end()), ret;
-}
-
-std::uint64_t primitive_root(std::uint64_t p) {
-  assert(is_prime(p));
-  auto divs = divisors(p - 1);
-  __uint128_t g = 2, pw, e, b;
-  for (bool isok = true;; g++, isok = true) {
-    for (int i = divs.size() - 2; i && isok; i--, isok &= pw != 1)
-      for (pw = 1, e = divs[i], b = g; e; e >>= 1, b = b * b % p)
-        if (e & 1) pw = pw * b % p;
-    if (isok) return g;
+constexpr auto totient(std::uint64_t n) { return totient(Factors(n)); }
+constexpr std::uint64_t carmichael(const Factors &f) {
+  std::uint64_t ret = 1, i = 0, tmp = 1;
+  for (const auto &[p, e] : f) {
+    for (tmp = p - 1, i = e - (p == 2 && e > 2); --i;) tmp *= p;
+    ret = std::lcm(ret, tmp);
   }
+  return ret;
 }
+constexpr auto carmichael(std::uint64_t n) { return carmichael(Factors(n)); }
+#endif

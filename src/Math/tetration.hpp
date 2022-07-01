075bf6b009d383ac0c3d086d72ad83345e188839
hashiryo <hashimotoryoma@gmail.com>
2022-07-02 08:52:47 +0900

    modify tetration

--- src/Math/tetration.hpp
+++ src/Math/tetration.hpp
@@ -1,6 +1,6 @@
 #pragma once
 #include <bits/stdc++.h>
-#include "src/Math/factorize.hpp"
+#include "src/Math/Factors.hpp"
 /**
  * @title テトレーション $H_4(a,b) = a\upuparrows b = a^{a^{\cdot^{\cdot^{a}}}}$
  * @category 数学
@@ -10,14 +10,17 @@
 // https://atcoder.jp/contests/summerfes2018-div1/tasks/summerfes2018_f
 
 // BEGIN CUT HERE
-
 constexpr std::uint64_t rec(std::uint64_t a, std::uint64_t b, std::uint64_t m) {
   if (a == 0) return (b ^ 1) & 1;
   if (b == 0 || m == 1) return 1;
+  std::uint64_t ret = 1, k = 1, tmp = 1, i = 0;
+  for (const auto &[p, e] : Factors(m)) {
+    for (tmp = p - 1, i = e - (p == 2 && e > 3); --i;) tmp *= p;
+    k = std::lcm(k, tmp);
+  }
   auto mod = [m](__uint128_t x) { return x < m ? x : x % m + m; };
-  std::uint64_t ret = 1, e = rec(a, b - 1, carmichael(m));
-  for (a = mod(a);; a = mod(__uint128_t(a) * a))
-    if (e& 1 ? ret = mod(__uint128_t(ret) * a) : 0; !(e >>= 1)) return ret;
+  for (k = rec(a, b - 1, k), a = mod(a);; a = mod(__uint128_t(a) * a))
+    if (k & 1 ? ret = mod(__uint128_t(ret) * a) : 0; !(k >>= 1)) return ret;
 }
 constexpr std::uint64_t H4(std::uint64_t a, std::uint64_t b, std::uint64_t m) {
   return (a = rec(a, b, m)) >= m ? a - m : a;

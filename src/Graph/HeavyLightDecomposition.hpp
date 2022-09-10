a6f1f0d775d3d5a52c773fa770004b14d95d74e6
hashiryo <hashimotoryoma@gmail.com>
2022-09-10 19:37:18 +0900

    add kd-tree

--- src/Graph/HeavyLightDecomposition.hpp
+++ src/Graph/HeavyLightDecomposition.hpp
@@ -12,21 +12,10 @@ struct HeavyLightDecomposition {
   std::vector<std::vector<int>> adj;
   std::vector<int> sz, in, out, head, par;
 
-  HeavyLightDecomposition(int n) : adj(n) {}
-  void add_edge(int u, int v) {
-    adj[u].push_back(v);
-    adj[v].push_back(u);
-  }
-  void build(int rt = 0) {
-    sz.assign(adj.size(), 1);
-    in.assign(adj.size(), 0);
-    out.assign(adj.size(), 0);
-    head.assign(adj.size(), 0);
-    par.assign(adj.size(), 0);
-    dfs_sz(rt, -1);
-    int t = 0;
-    dfs_hld(rt, -1, t);
-  }
+  HeavyLightDecomposition(int n)
+      : adj(n), sz(n, 1), in(n, 0), out(n, 0), head(n, 0), par(n, 0) {}
+  void add_edge(int u, int v) { adj[u].push_back(v), adj[v].push_back(u); }
+  void build(int rt = 0) { dfs_sz(rt, -1), dfs_hld(rt, -1); }
   int lca(int u, int v) const {
     for (;; v = par[head[v]]) {
       if (in[u] > in[v]) std::swap(u, v);
@@ -35,12 +24,11 @@ struct HeavyLightDecomposition {
   }
   template <typename T, typename Q, typename F>
   T fold_path(int u, int v, const Q &q, const F &f, const T ti,
-               bool edge = false) {
+              bool edge = false) {
     T vl = ti, vr = ti;
-    for (;; v = par[head[v]]) {
+    for (;; vl = f(q(in[head[v]], in[v] + 1), vl), v = par[head[v]]) {
       if (in[u] > in[v]) std::swap(u, v), std::swap(vl, vr);
       if (head[u] == head[v]) break;
-      vl = f(q(in[head[v]], in[v] + 1), vl);
     }
     return f(f(q(in[u] + edge, in[v] + 1), vl), vr);
   }
@@ -51,10 +39,9 @@ struct HeavyLightDecomposition {
 
   template <typename Q>
   void apply_path(int u, int v, const Q &q, bool edge = false) {
-    for (;; v = par[head[v]]) {
+    for (;; q(in[head[v]], in[v] + 1), v = par[head[v]]) {
       if (in[u] > in[v]) std::swap(u, v);
       if (head[u] == head[v]) break;
-      q(in[head[v]], in[v] + 1);
     }
     q(in[u] + edge, in[v] + 1);
   }
@@ -67,20 +54,15 @@ struct HeavyLightDecomposition {
   void dfs_sz(int v, int p) {
     if (adj[v].size() && adj[v][0] == p) std::swap(adj[v][0], adj[v].back());
     for (auto &u : adj[v])
-      if (u != p) {
-        dfs_sz(u, v);
-        sz[v] += sz[u];
-        if (sz[adj[v][0]] < sz[u]) std::swap(adj[v][0], u);
-      }
+      if (u != p)
+        if (dfs_sz(u, v), sz[v] += sz[u]; sz[adj[v][0]] < sz[u])
+          std::swap(adj[v][0], u);
   }
-  void dfs_hld(int v, int p, int &times) {
-    par[v] = p;
-    in[v] = times++;
+  void dfs_hld(int v, int p) {
+    static int times = 0;
+    par[v] = p, in[v] = times++;
     for (auto &u : adj[v])
-      if (u != p) {
-        head[u] = (adj[v][0] == u ? head[v] : u);
-        dfs_hld(u, v, times);
-      }
+      if (u != p) head[u] = (adj[v][0] == u ? head[v] : u), dfs_hld(u, v);
     out[v] = times;
   }
 };
---
+++ test/aoj/1068.KDT.test.cpp
@@ -0,0 +1,34 @@
+#define PROBLEM \
+  "https://onlinejudge.u-aizu.ac.jp/challenges/sources/UOA/UAPC/1068"
+#include <bits/stdc++.h>
+#include "src/DataStructure/KDTree.hpp"
+
+// 典型 2Dセグ木 minクエリ
+using namespace std;
+
+struct RminQ {
+  using T = int;
+  static T ti() { return INT_MAX; }
+  static T op(T l, T r) { return min(l, r); }
+};
+signed main() {
+  cin.tie(0);
+  ios::sync_with_stdio(0);
+  using KDT = KDTree<2, int, RminQ>;
+  for (int r, c, q; cin >> r >> c >> q && r != 0;) {
+    vector<typename KDT::PosVal> v;
+    for (int i = 0; i < r; i++)
+      for (int j = 0; j < c; j++) {
+        int grid;
+        cin >> grid;
+        v.emplace_back(array{i, j}, grid);
+      }
+    KDT kdt(v);
+    while (q--) {
+      int r1, c1, r2, c2;
+      cin >> r1 >> c1 >> r2 >> c2;
+      cout << kdt.fold({r1, r2}, {c1, c2}) << '\n';
+    }
+  }
+  return 0;
+}

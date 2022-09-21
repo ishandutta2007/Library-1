bc60bd5d26c76c457139cd8b4b1d6fc836e3db22
hashiryo <hashimotoryoma@gmail.com>
2022-09-21 14:39:26 +0900

    add new subproducttree

--- test/yosupo/multipoint_evaluation.test.cpp
+++ test/yosupo/multipoint_evaluation.test.cpp
@@ -1,23 +1,19 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/multipoint_evaluation"
 #include <bits/stdc++.h>
-#include "src/Old/FormalPowerSeries.hpp"
-#include "src/Old/ModInt.hpp"
-#include "src/Old/SubproductTree.hpp"
+#include "src/Math/ModInt.hpp"
+#include "src/FFT/SubProductTree.hpp"
 using namespace std;
 
 signed main() {
   cin.tie(0);
   ios::sync_with_stdio(0);
-  using Mint = ModInt<998244353>;
-  using FPS = FormalPowerSeries<Mint>;
+  using Mint = StaticModInt<998244353>;
   int N, M;
   cin >> N >> M;
-  FPS f(N);
-  vector<Mint> x(M);
-  for (int i = 0; i < N; i++) cin >> f[i];
-  for (int j = 0; j < M; j++) cin >> x[j];
-  vector<Mint> y = SubproductTree<Mint>(x).multi_eval(f);
-  for (int j = 0; j < M; j++) cout << (j ? " " : "") << y[j];
-  cout << endl;
+  std::vector<Mint> c(N), p(M);
+  for (int i = 0; i < N; i++) cin >> c[i];
+  for (int i = 0; i < M; i++) cin >> p[i];
+  auto ans = SubProductTree(p).multi_eval(c);
+  for (int i = 0; i < M; i++) cout << ans[i] << " \n"[i == M - 1];
   return 0;
 }
--- test/yosupo/polynomial_interpolation.test.cpp
+++ test/yosupo/polynomial_interpolation.test.cpp
@@ -1,22 +1,19 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/polynomial_interpolation"
 #include <bits/stdc++.h>
-#include "src/Old/ModInt.hpp"
-#include "src/Old/FormalPowerSeries.hpp"
-#include "src/Old/SubproductTree.hpp"
+#include "src/Math/ModInt.hpp"
+#include "src/FFT/SubProductTree.hpp"
 using namespace std;
 
 signed main() {
   cin.tie(0);
   ios::sync_with_stdio(0);
-  using Mint = ModInt<998244353>;
-  using FPS = FormalPowerSeries<Mint>;
+  using Mint = StaticModInt<998244353>;
   int N;
   cin >> N;
-  vector<Mint> x(N), y(N);
+  std::vector<Mint> x(N), y(N);
   for (int i = 0; i < N; i++) cin >> x[i];
   for (int i = 0; i < N; i++) cin >> y[i];
-  FPS f = SubproductTree<Mint>(x).interpolate(y);
-  for (int i = 0; i < N; i++) cout << (i ? " " : "") << f[i];
-  cout << endl;
+  auto ans = SubProductTree(x).interpolate(y);
+  for (int i = 0; i < N; i++) cout << ans[i] << " \n"[i == N - 1];
   return 0;
 }

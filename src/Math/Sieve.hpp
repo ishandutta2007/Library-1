6e7a7b8e3e7ac1fa36cee9ca48eaafe4101f4bfe
hashiryo <hashimotoryoma@gmail.com>
2022-09-21 11:13:45 +0900

    modify

--- src/Math/Sieve.hpp
+++ src/Math/Sieve.hpp
@@ -76,10 +76,7 @@ class Sieve {
   // O(N log k / log N + N)
   template <class T>
   static std::vector<T> pow_table(int N, std::uint64_t k) {
-    if (k == 0) {
-      std::vector<T> ret(N + 1, 0);
-      return ret[0] = 1, ret;
-    }
+    if (k == 0) return std::vector<T>(N + 1, 1);
     auto f = [k](int p, short) {
       T ret = 1, b = p;
       for (auto e = k;; b *= b)

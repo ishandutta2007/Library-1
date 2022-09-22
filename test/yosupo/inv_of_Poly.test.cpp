8ed6a3122bf02a2f5b54bffad3a7a89da208ca66
hashiryo <hashimotoryoma@gmail.com>
2022-09-23 00:53:48 +0900

    experiment

--- test/yosupo/inv_of_Poly.test.cpp
+++ test/yosupo/inv_of_Poly.test.cpp
@@ -15,7 +15,7 @@ signed main() {
   for (int i = 0; i < N; i++) cin >> f[i];
   for (int i = 0; i < M; i++) cin >> g[i];
   auto d = extgcd(f, g, x, y);
-  d.deg();
+  cerr << "d.deg() = " << d.deg() << '\n';
   if (d.deg() != 0) {
     cout << -1 << '\n';
   } else if (x.deg() == -1) {

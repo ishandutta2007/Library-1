0174b00317fe1ea35a785cac6e74bb7758f83b79
hashiryo <hashimotoryoma@gmail.com>
2022-09-23 03:05:54 +0900

    modify gcd

--- src/FFT/extgcd.hpp
+++ src/FFT/extgcd.hpp
@@ -18,49 +18,37 @@ Polynomial<mod_t, _Nm> extgcd(Polynomial<mod_t, _Nm> a,
   using Poly = Polynomial<mod_t, _Nm>;
   using PolyMat = std::array<Poly, 4>;
   assert(a.deg() >= 0), assert(b.deg() >= 0);
-  auto isI = [](const PolyMat &m) {
-    const mod_t ONE(1);
-    return m[1].deg() == -1 && m[2].deg() == -1 && m[0].deg() == 0 &&
-           m[0][0] == ONE && m[3].deg() == 0 && m[3][0] == ONE;
-  };
-  auto mul = [&](const PolyMat &l, const PolyMat &r) -> PolyMat {
-    if (isI(l)) return r;
-    if (isI(r)) return l;
-    return {l[0] * r[0] + l[1] * r[2], l[0] * r[1] + l[1] * r[3],
-            l[2] * r[0] + l[3] * r[2], l[2] * r[1] + l[3] * r[3]};
-  };
-  auto mulQ_l = [&](const Poly &q, const PolyMat &r) -> PolyMat {
-    return {r[2], r[3], r[0] - q * r[2], r[1] - q * r[3]};
-  };
-#define SUF(f, k) Poly(f.begin() + k, f.end())
+#define SUF(f, bg, ed) Poly(f.begin() + bg, f.begin() + ed)
   auto hgcd = [&](auto self, const Poly &p0, const Poly &p1) -> PolyMat {
-    assert(p0.deg() > p1.deg());
-    int m = ((p0.deg() - 1) >> 1) + 1, n = p1.deg();
-    if (n < m) return {mod_t(1), Poly(), Poly(), mod_t(1)};
-    PolyMat R = self(self, SUF(p0, m), SUF(p1, m));
+    int o = p0.deg(), m = ((o - 1) >> 1) + 1, n = p1.deg(), bdg, k, l;
+    if (assert(o > n); n < m) return {mod_t(1), Poly(), Poly(), mod_t(1)};
+    PolyMat R = self(self, SUF(p0, m, o + 1), SUF(p1, m, n + 1));
     Poly b = R[2] * p0 + R[3] * p1;
-    if (b.deg() < m) return R;
+    if (bdg = b.deg(); bdg < m) return R;
     std::pair<Poly, Poly> qr = (R[0] * p0 + R[1] * p1).quorem(b);
-    if (int k = 2 * m - b.deg(); (int)qr.second.size() > k)
-      return mul(self(self, SUF(b, k), SUF(qr.second, k)), mulQ_l(qr.first, R));
-    return mulQ_l(qr.first, R);
+    R = {R[2], R[3], R[0] - qr.first * R[2], R[1] - qr.first * R[3]};
+    if (k = 2 * m - bdg, l = m + 1, o = qr.second.size(); o < l) return R;
+    PolyMat A = self(self, SUF(b, k, bdg + 1), SUF(qr.second, k, o));
+    return {A[0] * R[0] + A[1] * R[2], A[0] * R[1] + A[1] * R[3],
+            A[2] * R[0] + A[3] * R[2], A[2] * R[1] + A[3] * R[3]};
   };
-  auto cogcd = [&](auto self, const Poly &p0, const Poly &p1) -> PolyMat {
+#undef SUF
+  auto cogcd = [&](auto self, const Poly &p0,
+                   const Poly &p1) -> std::pair<Poly, Poly> {
     assert(p0.deg() > p1.deg());
     PolyMat M = hgcd(hgcd, p0, p1);
     Poly p3 = M[2] * p0 + M[3] * p1;
-    if (p3.deg() == -1) return M;
+    if (p3.deg() == -1) return {M[0], M[1]};
     std::pair<Poly, Poly> qr = (M[0] * p0 + M[1] * p1).quorem(p3);
-    if (qr.second.deg() == -1) return mulQ_l(qr.first, M);
-    return mul(self(self, p3, qr.second), mulQ_l(qr.first, M));
+    if (qr.second.deg() == -1) return {M[2], M[3]};
+    auto A = self(self, p3, qr.second);
+    return {A.first * M[2] + A.second * (M[0] - qr.first * M[2]),
+            A.first * M[3] + A.second * (M[1] - qr.first * M[3])};
   };
-#undef SUF
-  if (a.shrink().size() <= b.shrink().size()) {
+  if (a.norm().size() <= b.norm().size()) {
     std::pair<Poly, Poly> qr = a.quorem(b);
-    PolyMat c = cogcd(cogcd, b, qr.second);
-    return a * (x = c[1]) + b * (y = c[0] - c[1] * qr.first);
-  } else {
-    PolyMat c = cogcd(cogcd, a, b);
-    return a * (x = c[0]) + b * (y = c[1]);
-  }
+    std::tie(y, x) = cogcd(cogcd, b, qr.second), y -= x * qr.first;
+  } else
+    std::tie(x, y) = cogcd(cogcd, a, b);
+  return a * x + b * y;
 }

73b7c2fbd84412f9350bc25288f9c088d432dfa2
hashiryo <hashimotoryoma@gmail.com>
2022-09-22 23:17:11 +0900

    modify gcd

--- src/FFT/extgcd.hpp
+++ src/FFT/extgcd.hpp
@@ -16,61 +16,52 @@ Polynomial<mod_t, _Nm> extgcd(Polynomial<mod_t, _Nm> a,
                               Polynomial<mod_t, _Nm> &x,
                               Polynomial<mod_t, _Nm> &y) {
   using Poly = Polynomial<mod_t, _Nm>;
-  using PVec = std::array<Poly, 2>;
-  using PMat = std::array<PVec, 2>;
+  using PMat = std::array<Poly, 4>;
   assert(a.deg() >= 0), assert(b.deg() >= 0);
   auto isI = [](const PMat &m) {
     const mod_t ONE(1);
-    return m[0][1].deg() == -1 && m[1][0].deg() == -1 && m[0][0].deg() == 0 &&
-           m[0][0][0] == ONE && m[1][1].deg() == 0 && m[1][1][0] == ONE;
+    return m[1].deg() == -1 && m[2].deg() == -1 && m[0].deg() == 0 &&
+           m[0][0] == ONE && m[3].deg() == 0 && m[3][0] == ONE;
   };
-  auto mulv = [&](const PMat &lhs, const PVec &rhs) {
-    if (isI(lhs)) return rhs;
-    return PVec{lhs[0][0] * rhs[0] + lhs[0][1] * rhs[1],
-                lhs[1][0] * rhs[0] + lhs[1][1] * rhs[1]};
+  auto mul = [&](const PMat &l, const PMat &r) -> PMat {
+    if (isI(l)) return r;
+    if (isI(r)) return l;
+    return {l[0] * r[0] + l[1] * r[2], l[0] * r[1] + l[1] * r[3],
+            l[2] * r[0] + l[3] * r[2], l[2] * r[1] + l[3] * r[3]};
   };
-  auto mul = [&](const PMat &lhs, const PMat &rhs) {
-    if (isI(lhs)) return rhs;
-    if (isI(rhs)) return lhs;
-    return PMat{PVec{lhs[0][0] * rhs[0][0] + lhs[0][1] * rhs[1][0],
-                     lhs[0][0] * rhs[0][1] + lhs[0][1] * rhs[1][1]},
-                PVec{lhs[1][0] * rhs[0][0] + lhs[1][1] * rhs[1][0],
-                     lhs[1][0] * rhs[0][1] + lhs[1][1] * rhs[1][1]}};
-  };
-  auto mulQ_l = [&](const Poly &q, const PMat &rhs) {
-    return PMat{PVec{rhs[1][0], rhs[1][1]},
-                PVec{rhs[0][0] - q * rhs[1][0], rhs[0][1] - q * rhs[1][1]}};
+  auto mulQ_l = [&](const Poly &q, const PMat &r) -> PMat {
+    return {r[2], r[3], r[0] - q * r[2], r[1] - q * r[3]};
   };
   auto hgcd = [&](auto self, const Poly &p0, const Poly &p1) -> PMat {
     assert(p0.deg() > p1.deg());
     int m = ((p0.deg() - 1) >> 1) + 1, n = p1.deg();
-    if (n < m) return PMat{PVec{Poly{1}, Poly()}, PVec{Poly(), Poly{1}}};
-    PMat R(self(self, Poly(p0.begin() + m, p0.end()),
-                Poly(p1.begin() + m, p1.end())));
-    PVec ab(mulv(R, PVec{p0, p1}));
-    if (ab[1].deg() < m) return R;
-    std::pair<Poly, Poly> qr(ab[0].quorem(ab[1]));
-    int k = 2 * m - ab[1].deg();
+    if (n < m) return {Poly{1}, Poly(), Poly(), Poly{1}};
+    PMat R = self(self, Poly(p0.begin() + m, p0.end()),
+                  Poly(p1.begin() + m, p1.end()));
+    Poly b = R[2] * p0 + R[3] * p1;
+    if (b.deg() < m) return R;
+    std::pair<Poly, Poly> qr = (R[0] * p0 + R[1] * p1).quorem(b);
+    int k = 2 * m - b.deg();
     if ((int)qr.second.size() <= k) return mulQ_l(qr.first, R);
-    return mul(self(self, Poly(ab[1].begin() + k, ab[1].end()),
+    return mul(self(self, Poly(b.begin() + k, b.end()),
                     Poly(qr.second.begin() + k, qr.second.end())),
                mulQ_l(qr.first, R));
   };
   auto cogcd = [&](auto self, const Poly &p0, const Poly &p1) -> PMat {
     assert(p0.deg() > p1.deg());
-    PMat M(hgcd(hgcd, p0, p1));
-    PVec p2p3(mulv(M, PVec{p0, p1}));
-    if (p2p3[1].deg() == -1) return M;
-    std::pair<Poly, Poly> qr(p2p3[0].quorem(p2p3[1]));
+    PMat M = hgcd(hgcd, p0, p1);
+    Poly p3 = M[2] * p0 + M[3] * p1;
+    if (p3.deg() == -1) return M;
+    std::pair<Poly, Poly> qr = (M[0] * p0 + M[1] * p1).quorem(p3);
     if (qr.second.deg() == -1) return mulQ_l(qr.first, M);
-    return mul(self(self, p2p3[1], qr.second), mulQ_l(qr.first, M));
+    return mul(self(self, p3, qr.second), mulQ_l(qr.first, M));
   };
   if (a.shrink().size() <= b.shrink().size()) {
-    std::pair<Poly, Poly> qr(a.quorem(b));
-    PMat c(cogcd(cogcd, b, qr.second));
-    return a * (x = c[0][1]) + b * (y = c[0][0] - c[0][1] * qr.first);
+    std::pair<Poly, Poly> qr = a.quorem(b);
+    PMat c = cogcd(cogcd, b, qr.second);
+    return a * (x = c[1]) + b * (y = c[0] - c[1] * qr.first);
   } else {
-    PMat c(cogcd(cogcd, a, b));
-    return a * (x = c[0][0]) + b * (y = c[0][1]);
+    PMat c = cogcd(cogcd, a, b);
+    return a * (x = c[0]) + b * (y = c[1]);
   }
 }

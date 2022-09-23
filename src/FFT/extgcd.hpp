70c2b0846df6908b470d142da8c5b9e294a24795
hashiryo <hashimotoryoma@gmail.com>
2022-09-23 14:15:59 +0900

    modify gcd

--- src/FFT/extgcd.hpp
+++ src/FFT/extgcd.hpp
@@ -20,15 +20,15 @@ Polynomial<mod_t, _Nm> extgcd(const Polynomial<mod_t, _Nm> &a,
   assert(a.deg() >= 0), assert(b.deg() >= 0);
 #define SUF(f, bg, ed) Poly(f.begin() + bg, f.begin() + ed)
   auto hgcd = [&](auto self, const Poly &p0, const Poly &p1) -> PolyMat {
-    int o = p0.deg(), m = ((o - 1) >> 1) + 1, n = p1.deg(),
-        k = (o + m - 1) >> 1;
+    int o = p0.deg(), m = ((o - 1) / 2) + 1, n = p1.deg(), k = (o + m - 1) / 2;
     if (assert(o > n); n <= k) return {Poly(), mod_t(1), mod_t(1), -p0 / p1};
     PolyMat R = self(self, SUF(p0, m, o + 1), SUF(p1, m, n + 1));
-    Poly b = R[2] * p0 + R[3] * p1;
-    std::pair<Poly, Poly> qr = (R[0] * p0 + R[1] * p1).quorem(b);
-    R = {R[2], R[3], R[0] - qr.first * R[2], R[1] - qr.first * R[3]};
-    if (n = b.deg(), k = 2 * m - n, o = qr.second.size(); o <= m) return R;
-    PolyMat A = self(self, SUF(b, k, n + 1), SUF(qr.second, k, o));
+    Poly p3 = R[2] * p0 + R[3] * p1;
+    std::pair<Poly, Poly> qr = (R[0] * p0 + R[1] * p1).quorem(p3);
+    R[0].swap(R[2]), R[1].swap(R[3]), R[2] -= qr.first * R[0],
+        R[3] -= qr.first * R[1];
+    if (n = p3.deg(), k = 2 * m - n, o = qr.second.size(); o <= m) return R;
+    PolyMat A = self(self, SUF(p3, k, n + 1), SUF(qr.second, k, o));
     return {A[0] * R[0] + A[1] * R[2], A[0] * R[1] + A[1] * R[3],
             A[2] * R[0] + A[3] * R[2], A[2] * R[1] + A[3] * R[3]};
   };

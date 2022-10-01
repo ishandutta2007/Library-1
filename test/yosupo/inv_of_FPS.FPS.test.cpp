dbe4c99db480510b9264c85f8d25ebea2d455eab
hashiryo <hashimotoryoma@gmail.com>
2022-10-01 16:28:58 +0900

    add FPS

---
+++ test/yosupo/inv_of_FPS.FPS.test.cpp
@@ -0,0 +1,19 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/inv_of_formal_power_series"
+#include <bits/stdc++.h>
+#include "src/Math/ModInt.hpp"
+#include "src/FFT/FormalPowerSeries.hpp"
+using namespace std;
+
+signed main() {
+  cin.tie(0);
+  ios::sync_with_stdio(0);
+  using Mint = StaticModInt<998244353>;
+  using FPS = FormalPowerSeries<Mint>;
+  int N;
+  cin >> N;
+  std::vector<Mint> A(N);
+  for (int i = 0; i < N; i++) cin >> A[i];
+  auto ans = FPS(A).inv();
+  for (int i = 0; i < N; i++) cout << ans[i] << " \n"[i == N - 1];
+  return 0;
+}

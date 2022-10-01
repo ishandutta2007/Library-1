c20f9c928e0b333fc0d12214f12001ed88468e0c
hashiryo <hashimotoryoma@gmail.com>
2022-10-01 17:05:00 +0900

    modify fps

---
+++ test/yukicoder/963.FPS.test.cpp
@@ -0,0 +1,20 @@
+#define PROBLEM "https://yukicoder.me/problems/no/963"
+#include <bits/stdc++.h>
+#include "src/Math/ModInt.hpp"
+#include "src/FFT/FormalPowerSeries.hpp"
+using namespace std;
+
+signed main() {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  using Mint = StaticModInt<1012924417>;
+  using FPS = FormalPowerSeries<Mint>;
+  FPS f;
+  f.reset().set(integ((f * f + 1) / 2) + 1);
+  int N;
+  cin >> N;
+  Mint ans = f[N] * 2;
+  for (int i = N; i; i--) ans *= i;
+  cout << ans << '\n';
+  return 0;
+}

9862cb02f1048abedc229f588443b5605ff1c669
hashiryo <hashimotoryoma@gmail.com>
2022-11-11 17:34:01 +0900

    add polymatprod

---
+++ test/yukicoder/502.test.cpp
@@ -0,0 +1,20 @@
+#define PROBLEM "https://yukicoder.me/problems/no/502"
+#include <bits/stdc++.h>
+#include "src/Math/ModInt.hpp"
+#include "src/FFT/polynomial_matrix_prod.hpp"
+using namespace std;
+
+int main() {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  static constexpr int MOD = 1e9 + 7;
+  using Mint = StaticModInt<MOD>;
+  long long n;
+  cin >> n;
+  if (n >= MOD) {
+    cout << 0 << '\n';
+  } else {
+    cout << polynomial_matrix_prod<Mint>({{{1, 1}}}, n)[0][0] << '\n';
+  }
+  return 0;
+}

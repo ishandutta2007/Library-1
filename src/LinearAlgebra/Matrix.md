4e432356a37d3a7ee9611f385cee54a75fa77066
hashiryo <hashimotoryoma@gmail.com>
2022-12-31 21:46:20 +0900

    add md

---
+++ src/LinearAlgebra/Matrix.md
@@ -0,0 +1,7 @@
+---
+title: 行列
+tation_of: ./Matrix.hpp
+---
+半環が載る
+## 問題例
+[AtCoder Regular Contest 027 D - ぴょんぴょんトレーニング](https://atcoder.jp/contests/arc027/tasks/arc027_4) セグ木に乗せる(MLギリギリ)
---
+++ test/yosupo/sqrt_of_sparse_FPS.test.cpp
@@ -0,0 +1,23 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/sqrt_of_formal_power_series_sparse"
+#include <bits/stdc++.h>
+#include "src/Math/ModInt.hpp"
+#include "src/Math/sparse_fps.hpp"
+using namespace std;
+int main() {
+ cin.tie(0);
+ ios::sync_with_stdio(false);
+ using Mint= StaticModInt<998244353>;
+ long long N, K;
+ cin >> N >> K;
+ vector<Mint> a(N);
+ while (K--) {
+  int i;
+  cin >> i;
+  cin >> a[i];
+ }
+ auto ans= sparse_sqrt(a, N);
+ if (ans.empty()) cout << -1 << '\n';
+ else
+  for (int i= 0; i < N; i++) cout << ans[i] << " \n"[i == N - 1];
+ return 0;
+}

f098cced39415c1fefd10ac5bd3c49822feb3c74
hashiryo <hashimotoryoma@gmail.com>
2022-12-31 23:16:37 +0900

    add md

--- src/Graph/LinearSystemIncidence.hpp
+++ src/Graph/LinearSystemIncidence.hpp
@@ -1,49 +1,28 @@
 #pragma once
 #include <bits/stdc++.h>
-/**
- * @title 接続行列の連立方程式
- * @category グラフ
- * @see https://opt-cp.com/linear-system-incidence/
- * O(V+E)
- */
-
-// verify用:
-// https://atcoder.jp/contests/abc155/tasks/abc155_f
-// https://atcoder.jp/contests/agc035/tasks/agc035_b
-// https://atcoder.jp/contests/cf17-final/tasks/cf17_final_e
-
-// BEGIN CUT HERE
-
-template <typename T>
-class LinearSystemIncidence {
-  int m;
-  std::vector<T> x;
-  std::vector<char> used;
-  std::vector<std::vector<std::tuple<int, int, bool>>> adj;
-  T dfs(const std::vector<T> &b, int u) {
-    used[u] = true;
-    T ret = b[u];
-    for (auto [id, to, fwd] : adj[u])
-      if (!used[to]) {
-        T tmp = dfs(b, to);
-        if constexpr (std::is_same_v<T, bool>)
-          x[id] = tmp, ret ^= tmp;
-        else
-          x[id] = fwd ? tmp : -tmp, ret += tmp;
-      }
-    return ret;
-  }
-
- public:
-  LinearSystemIncidence(int n) : m(0), adj(n) {}
-  void add_edge(int src, int dst) {
-    adj[src].emplace_back(m, dst, true), adj[dst].emplace_back(m++, src, false);
-  }
-  std::vector<T> solve(const std::vector<T> &b) {
-    x.assign(m, T(0)), used.assign(adj.size(), false);
-    for (std::size_t u = 0; u < adj.size(); u++)
-      if (!used[u] && dfs(b, u) != T(0))
-        return std::vector<T>();  // no sloution
-    return std::move(x);
-  }
+template <typename T> class LinearSystemIncidence {
+ int m;
+ std::vector<T> x;
+ std::vector<char> used;
+ std::vector<std::vector<std::tuple<int, int, bool>>> adj;
+ T dfs(const std::vector<T> &b, int u) {
+  used[u]= true;
+  T ret= b[u];
+  for (auto [id, to, fwd]: adj[u])
+   if (!used[to]) {
+    T tmp= dfs(b, to);
+    if constexpr (std::is_same_v<T, bool>) x[id]= tmp, ret^= tmp;
+    else x[id]= fwd ? tmp : -tmp, ret+= tmp;
+   }
+  return ret;
+ }
+public:
+ LinearSystemIncidence(int n): m(0), adj(n) {}
+ void add_edge(int src, int dst) { adj[src].emplace_back(m, dst, true), adj[dst].emplace_back(m++, src, false); }
+ std::vector<T> solve(const std::vector<T> &b) {
+  x.assign(m, T(0)), used.assign(adj.size(), false);
+  for (std::size_t u= 0; u < adj.size(); u++)
+   if (!used[u] && dfs(b, u) != T(0)) return std::vector<T>();  // no sloution
+  return std::move(x);
+ }
 };
---
+++ src/Graph/LinearSystemIncidence.md
@@ -0,0 +1,10 @@
+---
+title: 接続行列の連立方程式
+documentation_of: ./LinearSystemIncidence.hpp
+---
+## 計算量
+$\mathcal{O}(V+E)$
+## 問題例
+[AtCoder Beginner Contest 155 F - Perils in Parallel](https://atcoder.jp/contests/abc155/tasks/abc155_f) \
+[AtCoder Grand Contest 035 B - Even Degrees ](https://atcoder.jp/contests/agc035/tasks/agc035_b) \
+[CODE FESTIVAL 2017 Final E - Combination Lock](https://atcoder.jp/contests/cf17-final/tasks/cf17_final_e)

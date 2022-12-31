fceaaf0438a9a5382a757ebdd4fc0eaf465f418a
hashiryo <hashimotoryoma@gmail.com>
2022-12-31 22:54:50 +0900

    add md

--- src/DataStructure/BinaryIndexedTree_2D.hpp
+++ src/DataStructure/BinaryIndexedTree_2D.hpp
@@ -1,31 +1,20 @@
 #pragma once
 #include <bits/stdc++.h>
-/**
- * @title Binary-Indexed-Tree(2次元)
- * @category データ構造
- * O(logHW)
- * 1-indexed
- */
-
-// BEGIN CUT HERE
-
-template <typename T>
-struct BinaryIndexedTree_2D {
-  using vT = std::vector<T>;
-  std::vector<vT> dat;
-  BinaryIndexedTree_2D(int H, int W)
-      : dat(std::vector<vT>(H + 1, vT(W + 1, T(0)))) {}
-  void add(int y, int x, T v) {
-    for (int i = y; i < (int)dat.size(); i += i & -i)
-      for (int j = x; j < (int)dat[i].size(); j += j & -j) dat[i][j] += v;
-  }
-  T sum(int y, int x) {  // sum (0,y] * (0,x]
-    T ret = 0;
-    for (int i = y; i > 0; i -= i & -i)
-      for (int j = x; j > 0; j -= j & -j) ret += dat[i][j];
-    return ret;
-  }
-  T sum(int sy, int sx, int ty, int tx) {  // sum (sy,ty] * (sx,tx]
-    return sum(ty, tx) - sum(ty, sx) - sum(sy, tx) + sum(sy, sx);
-  }
+template <typename T> struct BinaryIndexedTree_2D {
+ using vT= std::vector<T>;
+ std::vector<vT> dat;
+ BinaryIndexedTree_2D(int H, int W): dat(std::vector<vT>(H + 1, vT(W + 1, T(0)))) {}
+ void add(int y, int x, T v) {
+  for (int i= y; i < (int)dat.size(); i+= i & -i)
+   for (int j= x; j < (int)dat[i].size(); j+= j & -j) dat[i][j]+= v;
+ }
+ T sum(int y, int x) {  // sum (0,y] * (0,x]
+  T ret= 0;
+  for (int i= y; i > 0; i-= i & -i)
+   for (int j= x; j > 0; j-= j & -j) ret+= dat[i][j];
+  return ret;
+ }
+ T sum(int sy, int sx, int ty, int tx) {  // sum (sy,ty] * (sx,tx]
+  return sum(ty, tx) - sum(ty, sx) - sum(sy, tx) + sum(sy, sx);
+ }
 };
--- src/DataStructure/UnionFind.hpp
+++ src/DataStructure/UnionFind.hpp
@@ -1,25 +1,14 @@
 #pragma once
 #include <bits/stdc++.h>
-/**
- * @title Union-Find
- * @category データ構造
- * @brief unite(x,y) x が入っている集合と y が入っている集合を併合する．
- * @brief same(x,y) x と y が同じ集合に入っているかどうかを判定する．
- * @brief O(α(N))
- */
-
-// BEGIN CUT HERE
-
 struct UnionFind {
-  std::vector<int> par;
-  UnionFind(int size) : par(size, -1) {}
-  bool unite(int u, int v) {
-    if ((u = root(u)) == (v = root(v))) return false;
-    if (par[u] > par[v]) std::swap(u, v);
-    par[u] += par[v], par[v] = u;
-    return true;
-  }
-  bool same(int u, int v) { return root(u) == root(v); }
-  int root(int u) { return par[u] < 0 ? u : par[u] = root(par[u]); }
-  int size(int u) { return -par[root(u)]; }
+ std::vector<int> par;
+ UnionFind(int size): par(size, -1) {}
+ bool unite(int u, int v) {
+  if ((u= root(u)) == (v= root(v))) return false;
+  if (par[u] > par[v]) std::swap(u, v);
+  return par[u]+= par[v], par[v]= u, true;
+ }
+ bool same(int u, int v) { return root(u) == root(v); }
+ int root(int u) { return par[u] < 0 ? u : par[u]= root(par[u]); }
+ int size(int u) { return -par[root(u)]; }
 };

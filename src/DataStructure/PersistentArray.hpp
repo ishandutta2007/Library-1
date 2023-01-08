fd6c8a02a32f07ba215179ec985ce5df879eba3b
hashiryo <hashimotoryoma@gmail.com>
2023-01-08 18:54:29 +0900

    add md

--- src/DataStructure/PersistentArray.hpp
+++ src/DataStructure/PersistentArray.hpp
@@ -1,48 +1,28 @@
 #pragma once
 #include <bits/stdc++.h>
-/**
- * @title 永続配列
- * @category データ構造
- * M分木
- * get: O(log_M N)
- * at: O(M log_M N)
- */
-
-// BEGIN CUT HERE
-
-template <class T, std::size_t M = 8>
-class PersistentArray {
-  struct Node {
-    T val;
-    Node *ch[M];
-  } * root;
-  T get(Node *&t, std::size_t k) {
-    return t ? (k ? get(t->ch[(k - 1) % M], (k - 1) / M) : t->val) : T();
-  }
-  bool is_null(Node *&t, std::size_t k) {
-    return t ? (k ? is_null(t->ch[(k - 1) % M], (k - 1) / M) : false) : true;
-  }
-  template <bool persistent = true>
-  T &at(Node *&t, std::size_t k) {
-    if (!t)
-      t = new Node();
-    else if constexpr (persistent)
-      t = new Node(*t);
-    return k ? at<persistent>(t->ch[(k - 1) % M], (k - 1) / M) : t->val;
-  }
-
- public:
-  PersistentArray() : root(nullptr) {}
-  PersistentArray(std::size_t n, T v) : root(nullptr) {
-    for (std::size_t i = n; i--;) at<false>(root, i) = v;
-  }
-  PersistentArray(T *bg, T *ed) : root(nullptr) {
-    for (std::size_t i = ed - bg; i--;) at<false>(root, i) = *(bg + i);
-  }
-  PersistentArray(const std::vector<T> &ar)
-      : PersistentArray(ar.data(), ar.data() + ar.size()) {}
-  bool is_null(std::size_t k) { return is_null(root, k); }
-  T get(std::size_t k) { return get(root, k); }
-  T &at(std::size_t k) { return at<true>(root, k); }
-  T &operator[](std::size_t k) { return at(k); }
+template <class T, std::size_t M= 8> class PersistentArray {
+ struct Node {
+  T val;
+  Node *ch[M];
+ } * root;
+ T get(Node *&t, std::size_t k) { return t ? (k ? get(t->ch[(k - 1) % M], (k - 1) / M) : t->val) : T(); }
+ bool is_null(Node *&t, std::size_t k) { return t ? (k ? is_null(t->ch[(k - 1) % M], (k - 1) / M) : false) : true; }
+ template <bool persistent= true> T &at(Node *&t, std::size_t k) {
+  if (!t) t= new Node();
+  else if constexpr (persistent) t= new Node(*t);
+  return k ? at<persistent>(t->ch[(k - 1) % M], (k - 1) / M) : t->val;
+ }
+public:
+ PersistentArray(): root(nullptr) {}
+ PersistentArray(std::size_t n, T v): root(nullptr) {
+  for (std::size_t i= n; i--;) at<false>(root, i)= v;
+ }
+ PersistentArray(T *bg, T *ed): root(nullptr) {
+  for (std::size_t i= ed - bg; i--;) at<false>(root, i)= *(bg + i);
+ }
+ PersistentArray(const std::vector<T> &ar): PersistentArray(ar.data(), ar.data() + ar.size()) {}
+ bool is_null(std::size_t k) { return is_null(root, k); }
+ T get(std::size_t k) { return get(root, k); }
+ T &at(std::size_t k) { return at<true>(root, k); }
+ T &operator[](std::size_t k) { return at(k); }
 };
---
+++ src/DataStructure/PersistentArray.md
@@ -0,0 +1,8 @@
+---
+title: 永続配列
+documentation_of: ./PersistentArray.hpp
+---
+M分木
+## 計算量
+get: $\mathcal{O}(\log_M N)$ \
+at: $\mathcal{O}(M \log_M N)$

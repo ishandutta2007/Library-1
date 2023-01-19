b2c726a6888636b80c320845a0a91a6f94444da3
hashiryo <hashimotoryoma@gmail.com>
2023-01-19 15:55:08 +0900

    modify

--- test/yosupo/partition.test.cpp
+++ test/yosupo/partition.test.cpp
@@ -4,14 +4,13 @@
 #include "src/Math/ModInt.hpp"
 #include "src/FFT/sequences.hpp"
 using namespace std;
-
 signed main() {
-  cin.tie(0);
-  ios::sync_with_stdio(0);
-  using Mint = StaticModInt<998244353>;
-  int N;
-  cin >> N;
-  auto ans = partition<Mint>(N);
-  for (int i = 0; i <= N; i++) cout << ans[i] << " \n"[i == N];
-  return 0;
+ cin.tie(0);
+ ios::sync_with_stdio(0);
+ using Mint= ModInt<998244353>;
+ int N;
+ cin >> N;
+ auto ans= partition<Mint>(N);
+ for (int i= 0; i <= N; i++) cout << ans[i] << " \n"[i == N];
+ return 0;
 }

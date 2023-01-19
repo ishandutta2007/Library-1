d48c90c91f2c8b074c7280a78fd453537bc3ff15
hashiryo <hashimotoryoma@gmail.com>
2023-01-19 12:48:01 +0900

    modify

--- test/yosupo/range_affine_range_sum.DynSeg.test.cpp
+++ test/yosupo/range_affine_range_sum.DynSeg.test.cpp
@@ -8,39 +8,34 @@
 
 using namespace std;
 
-using Mint = StaticModInt<998244353>;
+using Mint= ModInt<998244353>;
 struct RaffineQ_RsumQ {
-  using T = Mint;
-  using E = pair<Mint, Mint>;
-  static T ti() { return 0; }
-  static T op(const T &l, const T &r) { return l + r; }
-  static void mapping(T &v, const E &f, int sz) {
-    v = f.first * v + f.second * sz;
-  }
-  static void composition(E &pre, const E &suf) {
-    pre = {suf.first * pre.first, suf.first * pre.second + suf.second};
-  }
+ using T= Mint;
+ using E= pair<Mint, Mint>;
+ static T ti() { return 0; }
+ static T op(const T &l, const T &r) { return l + r; }
+ static void mapping(T &v, const E &f, int sz) { v= f.first * v + f.second * sz; }
+ static void composition(E &pre, const E &suf) { pre= {suf.first * pre.first, suf.first * pre.second + suf.second}; }
 };
-
 signed main() {
-  cin.tie(0);
-  ios::sync_with_stdio(0);
-  int N, Q;
-  cin >> N >> Q;
-  Mint a[N];
-  for (int i = 0; i < N; i++) cin >> a[i];
-  SegmentTree_Dynamic<RaffineQ_RsumQ> seg(a, a + N);
-  while (Q--) {
-    bool op;
-    int l, r;
-    cin >> op >> l >> r;
-    if (op) {
-      cout << seg.fold(l, r) << endl;
-    } else {
-      Mint b, c;
-      cin >> b >> c;
-      seg.apply(l, r, {Mint(b), Mint(c)});
-    }
+ cin.tie(0);
+ ios::sync_with_stdio(0);
+ int N, Q;
+ cin >> N >> Q;
+ Mint a[N];
+ for (int i= 0; i < N; i++) cin >> a[i];
+ SegmentTree_Dynamic<RaffineQ_RsumQ> seg(a, a + N);
+ while (Q--) {
+  bool op;
+  int l, r;
+  cin >> op >> l >> r;
+  if (op) {
+   cout << seg.fold(l, r) << endl;
+  } else {
+   Mint b, c;
+   cin >> b >> c;
+   seg.apply(l, r, {Mint(b), Mint(c)});
   }
-  return 0;
+ }
+ return 0;
 }

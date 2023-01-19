c0214f9ec4c136975ee4d8cb21df39d1e6e28d0d
hashiryo <hashimotoryoma@gmail.com>
2023-01-19 17:18:16 +0900

    modify

--- test/yukicoder/137.div_at.test.cpp
+++ test/yukicoder/137.div_at.test.cpp
@@ -4,20 +4,19 @@
 #include "src/FFT/convolve.hpp"
 #include "src/FFT/bostan_mori.hpp"
 using namespace std;
-
 signed main() {
-  cin.tie(0);
-  ios::sync_with_stdio(0);
-  using Mint = StaticModInt<1234567891>;
-  long long N, M;
-  cin >> N >> M;
-  std::vector<Mint> f = {1};
-  for (int i = 0; i < N; i++) {
-    int A;
-    cin >> A;
-    std::vector<Mint> g(A + 1);
-    g[0] = 1, g[A] = -1, f = convolve(f, g);
-  }
-  cout << div_at<Mint>({1}, f, M) << '\n';
-  return 0;
+ cin.tie(0);
+ ios::sync_with_stdio(0);
+ using Mint= ModInt<1234567891>;
+ long long N, M;
+ cin >> N >> M;
+ std::vector<Mint> f= {1};
+ for (int i= 0; i < N; i++) {
+  int A;
+  cin >> A;
+  std::vector<Mint> g(A + 1);
+  g[0]= 1, g[A]= -1, f= convolve(f, g);
+ }
+ cout << div_at<Mint>({1}, f, M) << '\n';
+ return 0;
 }

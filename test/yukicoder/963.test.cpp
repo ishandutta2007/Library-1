a4e33846a7d46ce24b6026ec613277a6dc2aa3f2
hashiryo <hashimotoryoma@gmail.com>
2023-01-19 17:58:38 +0900

    modify

--- test/yukicoder/963.test.cpp
+++ test/yukicoder/963.test.cpp
@@ -3,13 +3,12 @@
 #include "src/Math/ModInt.hpp"
 #include "src/FFT/sequences.hpp"
 using namespace std;
-
 signed main() {
-  cin.tie(0);
-  ios::sync_with_stdio(0);
-  using Mint = StaticModInt<1012924417>;
-  int N;
-  cin >> N;
-  cout << alternating_permutation<Mint>(N)[N] * 2 << '\n';
-  return 0;
+ cin.tie(0);
+ ios::sync_with_stdio(0);
+ using Mint= ModInt<1012924417>;
+ int N;
+ cin >> N;
+ cout << alternating_permutation<Mint>(N)[N] * 2 << '\n';
+ return 0;
 }

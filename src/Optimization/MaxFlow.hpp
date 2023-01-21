cada33318fa005254416fbb29c1a513c2fd9733b
hashiryo <hashimotoryoma@gmail.com>
2023-01-22 00:08:35 +0900

    modify maxflow

--- src/Optimization/MaxFlow.hpp
+++ src/Optimization/MaxFlow.hpp
@@ -17,44 +17,42 @@ template <typename FlowAlgo> struct MaxFlow: public FlowAlgo {
  }
  struct EdgePtr {
   friend class MaxFlow;
-  MaxFlow *instance;
+  MaxFlow *ins;
   int v, e;
-  bool bidir;
-  Edge &edge() { return instance->adj[v][e]; }
+  bool bd;
+  Edge &edge() { return ins->adj[v][e]; }
   Edge &rev() {
    Edge &e= edge();
-   return instance->adj[e.dst][e.rev];
+   return ins->adj[e.dst][e.rev];
   }
-  EdgePtr(MaxFlow *instance, int v, int e, bool d): instance(instance), v(v), e(e), bidir(d) {}
+  EdgePtr(MaxFlow *ins, int v, int e, bool d): ins(ins), v(v), e(e), bd(d) {}
  public:
   EdgePtr()= default;
-  int src() { return v; }
+  int src() const { return v; }
   int dst() { return edge().dst; }
-  bool is_direct() const { return !bidir; }
+  bool is_direct() const { return !bd; }
   flow_t flow() { return cap() - edge().cap; }
-  flow_t cap() { return (edge().cap + rev().cap) / (1 + bidir); }
+  flow_t cap() { return (edge().cap + rev().cap) / (1 + bd); }
   flow_t change_cap(flow_t new_cap, int s, int t) {
    assert(0 <= new_cap);
    Edge &e= edge(), &re= rev();
-   flow_t diff= new_cap - cap(), extrusion= std::abs(flow()) - new_cap;
-   if (extrusion <= 0) return e.cap+= diff, re.cap+= diff * bidir, 0;
+   flow_t diff= new_cap - cap(), ext= std::abs(flow()) - new_cap;
+   if (ext <= 0) return e.cap+= diff, re.cap+= diff * bd, 0;
    int sr= src(), ds= dst();
    if (flow() < 0) std::swap(sr, ds);
-   if (bidir) {
+   if (bd) {
     if (sr == src()) re.cap+= 2 * diff + e.cap, e.cap= 0;
     else e.cap+= 2 * diff + re.cap, re.cap= 0;
    } else re.cap+= diff;
-   extrusion-= instance->maxflow(sr, ds, extrusion);
-   if (ds != t) instance->maxflow(t, ds, extrusion);
-   if (sr != s) instance->maxflow(sr, s, extrusion);
-   return extrusion;
+   ext-= ins->maxflow(sr, ds, ext);
+   if (ds != t) ins->maxflow(t, ds, ext);
+   if (sr != s) ins->maxflow(sr, s, ext);
+   return ext;
   }
  };
  EdgePtr add_edge(int src, int dst, flow_t cap, bool bidir= false) {
-  assert(0 <= src && src < this->n), assert(0 <= dst && dst < this->n);
-  assert(0 <= cap);
-  int e= this->adj[src].size();
-  int re= src == dst ? e + 1 : this->adj[dst].size();
+  assert(0 <= src && src < this->n), assert(0 <= dst && dst < this->n), assert(0 <= cap);
+  int e= this->adj[src].size(), re= src == dst ? e + 1 : this->adj[dst].size();
   return this->adj[src].push_back(Edge{dst, re, cap}), this->adj[dst].push_back(Edge{src, e, cap * bidir}), this->m++, EdgePtr{this, src, e, bidir};
  }
  flow_t maxflow(int s, int t) { return maxflow(s, t, std::numeric_limits<flow_t>::max()); }
@@ -75,8 +73,7 @@ template <typename FlowAlgo> class MaxFlowLowerBound: public FlowAlgo {
  using flow_t= decltype(Edge::cap);
  std::vector<flow_t> in;
  int add_edge(int src, int dst, flow_t cap) {
-  int e= this->adj[src].size();
-  int re= src == dst ? e + 1 : this->adj[dst].size();
+  int e= this->adj[src].size(), re= src == dst ? e + 1 : this->adj[dst].size();
   return this->adj[src].push_back(Edge{dst, re, cap}), this->adj[dst].push_back(Edge{src, e, 0}), this->m++, re;
  }
 public:
@@ -84,20 +81,19 @@ public:
  int add_vertex() { return this->adj.resize(++this->n), in.resize(this->n - 2, 0), this->n - 3; }
  std::vector<int> add_vertices(const std::size_t size) {
   std::vector<int> ret(size);
-  std::iota(ret.begin(), ret.end(), this->n - 2);
-  return this->adj.resize(this->n+= size), in.resize(this->n - 2, 0), ret;
+  return std::iota(ret.begin(), ret.end(), this->n - 2), this->adj.resize(this->n+= size), in.resize(this->n - 2, 0), ret;
  }
  struct EdgePtr {
   friend class MaxFlowLowerBound;
-  MaxFlowLowerBound *instance;
+  MaxFlowLowerBound *ins;
   int v, e;
   flow_t u;
-  const Edge &edge() { return instance->adj[v][e]; }
+  const Edge &edge() { return ins->adj[v][e]; }
   const Edge &rev() {
    Edge &e= edge();
-   return instance->adj[e.dst][e.rev];
+   return ins->adj[e.dst][e.rev];
   }
-  EdgePtr(MaxFlowLowerBound *instance, int v, int e, flow_t u): instance(instance), v(v), e(e), u(u) {}
+  EdgePtr(MaxFlowLowerBound *ins, int v, int e, flow_t u): ins(ins), v(v), e(e), u(u) {}
  public:
   EdgePtr()= default;
   int src() const { return v; }
@@ -107,22 +103,11 @@ public:
   flow_t upper() const { return u; }
  };
  EdgePtr add_edge(int src, int dst, flow_t lower, flow_t upper) {
-  assert(lower <= upper), src+= 2, dst+= 2;
-  assert(0 <= src && src < this->n), assert(0 <= dst && dst < this->n);
-  this->m++;
+  assert(lower <= upper), src+= 2, dst+= 2, assert(0 <= src && src < this->n), assert(0 <= dst && dst < this->n), ++this->m;
   int e= this->adj[src].size(), re= src == dst ? e + 1 : this->adj[dst].size();
-  if (lower * upper <= 0) {
-   this->adj[src].push_back(Edge{dst, re, upper});
-   this->adj[dst].push_back(Edge{src, e, -lower});
-  } else if (lower > 0) {
-   in[src - 2]-= lower, in[dst - 2]+= lower;
-   this->adj[src].push_back(Edge{dst, re, upper - lower});
-   this->adj[dst].push_back(Edge{src, e, 0});
-  } else {
-   in[src - 2]-= upper, in[dst - 2]+= upper;
-   this->adj[src].push_back(Edge{dst, re, 0});
-   this->adj[dst].push_back(Edge{src, e, upper - lower});
-  }
+  if (lower * upper <= 0) this->adj[src].push_back(Edge{dst, re, upper}), this->adj[dst].push_back(Edge{src, e, -lower});
+  else if (lower > 0) in[src - 2]-= lower, in[dst - 2]+= lower this->adj[src].push_back(Edge{dst, re, upper - lower}), this->adj[dst].push_back(Edge{src, e, 0});
+  else in[src - 2]-= upper, in[dst - 2]+= upper, this->adj[src].push_back(Edge{dst, re, 0}), this->adj[dst].push_back(Edge{src, e, upper - lower});
   return EdgePtr(this, src, e, upper);
  }
  flow_t maxflow(int s, int t) {
@@ -168,12 +153,11 @@ protected:
   }
   return level[u]= n, ret;
  }
- flow_t flow(int s, int t, flow_t flow_lim, flow_t ret= 0) {
+ flow_t flow(int s, int t, flow_t lim, flow_t ret= 0) {
   assert(0 <= s && s < n), assert(0 <= t && t < n), assert(s != t);
-  for (flow_t f; ret < flow_lim; ret+= f) {
+  for (flow_t f; ret < lim; ret+= f) {
    if (levelize(s, t), level[t] == -1) break;
-   iter.assign(n, 0);
-   if (!(f= dfs(t, s, flow_lim - ret))) break;
+   if (iter.assign(n, 0); !(f= dfs(t, s, lim - ret))) break;
   }
   return ret;
  }
@@ -187,26 +171,20 @@ protected:
  };
  int n, gap, m;
  struct {
-  std::vector<std::pair<int, int>> even, odd;
+  std::vector<int> ei, eh, oi, oh;
   int se, so;
-  void init(int _n) { even.resize(_n), odd.resize(_n), se= so= 0; };
+  void init(int _n) { ei.resize(_n), eh.resize(_n), oi.resize(_n), oh.resize(_n), se= so= 0; };
   void clear() { se= so= 0; }
   inline bool empty() const { return se + so == 0; }
-  void push(int i, int h) { (h & 1 ? odd[so++] : even[se++])= {i, h}; }
-  inline int highest() const {
-   int a= se ? even[se - 1].second : -1, b= so ? odd[so - 1].second : -1;
-   return a > b ? a : b;
-  }
-  inline int pop() {
-   if (!se || (so && odd[so - 1].second > even[se - 1].second)) return odd[--so].first;
-   return even[--se].first;
-  }
+  void push(int i, int h) { (h & 1 ? tie(oi[so], oh[so++]) : tie(ei[se], eh[se++]))= {i, h}; }
+  inline int highest() const { return max(se ? eh[se - 1] : -1, so ? oh[so - 1] : -1); }
+  inline int pop() { return !se || (so && oh[so - 1] > eh[se - 1]) ? oi[--so] : ei[--se]; }
  } hque;
  std::vector<std::vector<Edge>> adj;
  std::vector<int> dist, dcnt;
- std::vector<flow_t> excess;
+ std::vector<flow_t> exc;
  inline void calc(int t) {
-  if constexpr (global_freq != 0) global_relabeling(t);
+  if constexpr (global_freq != 0) relabel(t);
   for (int tick= m * global_freq; !hque.empty();) {
    int i= hque.pop(), dnxt= n * 2 - 1;
    if constexpr (use_gap)
@@ -214,10 +192,10 @@ protected:
    for (auto &e: adj[i])
     if (e.cap) {
      if (dist[e.dst] == dist[i] - 1) {
-      if (push(i, e), excess[i] == 0) break;
+      if (push(i, e), exc[i] == 0) break;
      } else if (dist[e.dst] + 1 < dnxt) dnxt= dist[e.dst] + 1;
     }
-   if (excess[i] > 0) {
+   if (exc[i] > 0) {
     if constexpr (use_gap) {
      if (dnxt != dist[i] && dcnt[dist[i]] == 1 && dist[i] < gap) gap= dist[i];
      if (dnxt == gap) gap++;
@@ -228,7 +206,7 @@ protected:
     dist[i]= dnxt, hq_push(i);
    }
    if constexpr (global_freq != 0)
-    if (--tick == 0) tick= m * global_freq, global_relabeling(t);
+    if (--tick == 0) tick= m * global_freq, relabel(t);
   }
  }
  inline void hq_push(int i) {
@@ -236,12 +214,10 @@ protected:
   else if (dist[i] < gap) hque.push(i, dist[i]);
  }
  inline void push(int i, Edge &e) {
-  flow_t delta= std::min(e.cap, excess[i]);
-  excess[i]-= delta, e.cap-= delta;
-  excess[e.dst]+= delta, adj[e.dst][e.rev].cap+= delta;
-  if (0 < excess[e.dst] && excess[e.dst] <= delta) hq_push(e.dst);
+  flow_t del= std::min(e.cap, exc[i]);
+  if (exc[i]-= del, e.cap-= del, exc[e.dst]+= del, adj[e.dst][e.rev].cap+= del; 0 < exc[e.dst] && exc[e.dst] <= del) hq_push(e.dst);
  }
- inline void global_relabeling(int t) {
+ inline void relabel(int t) {
   dist.assign(n, n), dist[t]= 0;
   static std::queue<int> q;
   q.push(t), hque.clear();
@@ -249,22 +225,20 @@ protected:
   for (int now; !q.empty();) {
    now= q.front(), q.pop();
    if constexpr (use_gap) gap= dist[now] + 1, dcnt[dist[now]]++;
-   if (excess[now] > 0) hque.push(now, dist[now]);
+   if (exc[now] > 0) hque.push(now, dist[now]);
    for (const auto &e: adj[now])
     if (adj[e.dst][e.rev].cap && dist[e.dst] == n) dist[e.dst]= dist[now] + 1, q.push(e.dst);
   }
  }
- flow_t flow(int s, int t, flow_t flow_lim, flow_t ret= 0) {
-  assert(0 <= s && s < n), assert(0 <= t && t < n), assert(s != t);
-  hque.init(n), excess.assign(n, 0), excess[s]+= flow_lim;
-  excess[t]-= flow_lim, dist.assign(n, 0), dist[s]= n;
+ flow_t flow(int s, int t, flow_t lim, flow_t ret= 0) {
+  assert(0 <= s && s < n), assert(0 <= t && t < n), assert(s != t), hque.init(n), exc.assign(n, 0), exc[s]+= lim, exc[t]-= lim, dist.assign(n, 0), dist[s]= n;
   if constexpr (use_gap) gap= 1, dcnt.assign(n + 1, 0), dcnt[0]= n - 1;
   for (auto &e: adj[s]) push(s, e);
-  calc(t), ret= excess[t] + flow_lim;
+  calc(t), ret= exc[t] + lim;
   if constexpr (!freeze) {
-   excess[s]+= excess[t], excess[t]= 0;
-   if constexpr (global_freq != 0) global_relabeling(s);
-   calc(s), assert(excess == std::vector<flow_t>(n, 0));
+   exc[s]+= exc[t], exc[t]= 0;
+   if constexpr (global_freq != 0) relabel(s);
+   calc(s), assert(exc == std::vector<flow_t>(n, 0));
   }
   return ret;
  }
--- test/aoj/1615.Dinic.test.cpp
+++ test/aoj/1615.Dinic.test.cpp
@@ -1,35 +1,32 @@
-#define PROBLEM \
-  "https://onlinejudge.u-aizu.ac.jp/challenges/sources/ICPC/Prelim/1615"
-#include <bits/stdc++.h>
-#include "src/Optimization/MaxFlow_Dinic.hpp"
-#include "src/Optimization/MaxFlowLowerBound.hpp"
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/challenges/sources/ICPC/Prelim/1615"
+#include <iostream>
+#include "src/Optimization/MaxFlow.hpp"
 using namespace std;
-
 signed main() {
-  cin.tie(0);
-  ios::sync_with_stdio(0);
-  for (int n, m; cin >> n >> m, n;) {
-    int u[m], v[m];
-    for (int i = 0; i < m; i++) cin >> u[i] >> v[i], u[i]--, v[i]--;
-    for (int d = 0; d <= n; d++)
-      for (int l = n - d; l >= 0; l--) {
-        int r = l + d;
-        MaxFlowLowerBound<Dinic<long long>> graph;
-        int s = graph.add_vertex(), t = graph.add_vertex();
-        auto e = graph.add_vertices(m);
-        auto w = graph.add_vertices(n);
-        for (int i = 0; i < m; i++) {
-          graph.add_edge(s, e[i], 0, 1);
-          graph.add_edge(e[i], w[u[i]], 0, 1);
-          graph.add_edge(e[i], w[v[i]], 0, 1);
-        }
-        for (int i = 0; i < n; i++) graph.add_edge(w[i], t, l, r);
-        if (graph.maxflow(s, t) == m) {
-          cout << l << " " << r << '\n';
-          d = n + 1;
-          break;
-        }
-      }
-  }
-  return 0;
+ cin.tie(0);
+ ios::sync_with_stdio(0);
+ for (int n, m; cin >> n >> m, n;) {
+  int u[m], v[m];
+  for (int i= 0; i < m; i++) cin >> u[i] >> v[i], u[i]--, v[i]--;
+  for (int d= 0; d <= n; d++)
+   for (int l= n - d; l >= 0; l--) {
+    int r= l + d;
+    MaxFlowLowerBound<Dinic<long long>> graph;
+    int s= graph.add_vertex(), t= graph.add_vertex();
+    auto e= graph.add_vertices(m);
+    auto w= graph.add_vertices(n);
+    for (int i= 0; i < m; i++) {
+     graph.add_edge(s, e[i], 0, 1);
+     graph.add_edge(e[i], w[u[i]], 0, 1);
+     graph.add_edge(e[i], w[v[i]], 0, 1);
+    }
+    for (int i= 0; i < n; i++) graph.add_edge(w[i], t, l, r);
+    if (graph.maxflow(s, t) == m) {
+     cout << l << " " << r << '\n';
+     d= n + 1;
+     break;
+    }
+   }
+ }
+ return 0;
 }

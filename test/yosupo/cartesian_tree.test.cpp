18ae0be569fc90ec31e30615496064e5e2e4cfcf
hashiryo <hashimotoryoma@gmail.com>
2023-02-28 17:13:21 +0900

    add cartesian tree

---
+++ test/yosupo/cartesian_tree.test.cpp
@@ -0,0 +1,16 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/cartesian_tree"
+#include <iostream>
+#include <vector>
+#include "src/Misc/CartesianTree.hpp"
+using namespace std;
+signed main() {
+ cin.tie(0);
+ ios::sync_with_stdio(0);
+ int N;
+ cin >> N;
+ vector<int> a(N);
+ for (int i= 0; i < N; ++i) cin >> a[i];
+ CartesianTree ct(a);
+ for (int i= 0; i < N; ++i) cout << (ct.parent(i) == -1 ? i : ct.parent(i)) << " \n"[i == N - 1];
+ return 0;
+}

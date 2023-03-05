38c27a6b4e7013c2cd5bb73f68e3e2a0e9f529ee
hashiryo <hashimotoryoma@gmail.com>
2023-03-05 16:57:48 +0900

    modify lsi

--- src/Graph/LinearSystemIncidence.hpp
+++ src/Graph/LinearSystemIncidence.hpp
@@ -4,34 +4,36 @@
 #include <array>
 #include <type_traits>
 template <typename T> class LinearSystemIncidence {
+ const int n;
  std::vector<std::array<int, 2>> es;
- std::vector<std::vector<int>> adj;
 public:
- LinearSystemIncidence(int n): adj(n) {}
- void add_edge(int src, int dst) {
-  int m= es.size();
-  adj[src].push_back(m), adj[dst].push_back(m), es.push_back({src, dst});
- }
+ LinearSystemIncidence(int n): n(n) {}
+ void add_edge(int src, int dst) { es.push_back({src, dst}); }
  std::vector<T> solve(std::vector<T> b) const {
-  const int n= adj.size();
-  std::vector<T> x(es.size());
-  std::vector<int> pre(n, -2), dat(n, 0);
+  assert((int)b.size() == n);
+  const int m= es.size();
+  std::vector<T> x(m);
+  std::vector<int> adj(m + m), pos(n + 1, 0);
+  for (auto [s, d]: es) ++pos[s], ++pos[d];
+  for (int i= 1; i <= n; ++i) pos[i]+= pos[i - 1];
+  for (int i= m; i--;) {
+   auto [s, d]= es[i];
+   adj[--pos[s]]= i, adj[--pos[d]]= i;
+  }
+  std::vector<int> pre(n, -2), dat(pos.begin(), pos.begin() + n);
   for (int s= 0, p, e, q, f; s < n; ++s)
    if (pre[s] == -2)
     for (pre[p= s]= -1;;) {
-     if (dat[p] == (int)adj[p].size()) {
+     if (dat[p] == pos[p + 1]) {
       if (e= pre[p]; e < 0) {
        if (b[p] != T()) return {};  // no solution
        break;
       }
-      f= (es[e][0] == p), q= es[e][f];
       T tmp= b[p];
-      if constexpr (std::is_same_v<T, bool>) x[e]= tmp, b[q]= tmp ^ b[q];
-      else x[e]= f ? -tmp : tmp, b[q]+= tmp;
-      p= q;
-      continue;
-     }
-     if (e= adj[p][dat[p]++], q= es[e][es[e][0] == p]; pre[q] == -2) pre[q]= e, p= q;
+      p= es[e][f= (es[e][0] == p)];
+      if constexpr (std::is_same_v<T, bool>) x[e]= tmp, b[p]= tmp ^ b[p];
+      else x[e]= f ? -tmp : tmp, b[p]+= tmp;
+     } else if (e= adj[dat[p]++], q= es[e][es[e][0] == p]; pre[q] == -2) pre[p= q]= e;
     }
   return x;
  }

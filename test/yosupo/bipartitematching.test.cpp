be0e585f1394b96c90908c7a0be4c98f6ecd4540
hashiryo <hashimotoryoma@gmail.com>
2023-03-07 15:16:22 +0900

    modify bm

--- test/yosupo/bipartitematching.test.cpp
+++ test/yosupo/bipartitematching.test.cpp
@@ -1,24 +1,21 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/bipartitematching"
 #include <iostream>
-#include "src/Graph/MatchingBipartite.hpp"
+#include "src/Graph/BipartiteMatching.hpp"
 using namespace std;
 signed main() {
  cin.tie(0);
  ios::sync_with_stdio(0);
  int L, R, M;
  cin >> L >> R >> M;
- MatchingBipartite graph(L, R);
+ BipartiteMatching bm(L, R);
  while (M--) {
   int a, b;
   cin >> a >> b;
-  graph.add_edge(a, b);
+  bm.add_edge(a, b);
  }
- auto ans= graph.get_matching();
- auto left= ans.second.first;
- cout << ans.first << '\n';
- for (int i= 0; i < (int)left.size(); i++)
-  if (left[i] != -1) {
-   cout << i << " " << left[i] << '\n';
-  }
+ bm.build();
+ auto ans= bm.max_matching();
+ cout << ans.size() << '\n';
+ for (auto [l, r]: ans) cout << l << " " << r << '\n';
  return 0;
 }

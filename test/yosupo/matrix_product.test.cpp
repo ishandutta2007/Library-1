f51542cc94a9a8104adc5ee00002ccc66cd86372
hashiryo <hashimotoryoma@gmail.com>
2023-03-17 18:15:59 +0900

    modify

---
+++ test/yosupo/matrix_product.test.cpp
@@ -0,0 +1,21 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/matrix_product"
+#include <iostream>
+#include "src/LinearAlgebra/Matrix.hpp"
+#include "src/Math/ModInt.hpp"
+using namespace std;
+signed main() {
+ cin.tie(0);
+ ios::sync_with_stdio(0);
+ using Mint= ModInt<998244353>;
+ int N, M, K;
+ cin >> N >> M >> K;
+ Matrix<Mint> A(N, M), B(M, K);
+ for (int i= 0; i < N; ++i)
+  for (int j= 0; j < M; +j) cin >> A[i][j];
+ for (int i= 0; i < M; ++i)
+  for (int j= 0; j < K; ++j) cin >> B[i][j];
+ auto C= A * B;
+ for (int i= 0; i < N; ++i)
+  for (int j= 0; j < K; ++j) cout << C[i][j] << " \n"[j == K - 1];
+ return 0;
+}

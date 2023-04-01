602b6b4f22f89c6fca09934c4a66b01412cd1c40
hashiryo <hashimotoryoma@gmail.com>
2023-04-02 01:58:46 +0900

    modify

--- src/Math/ModInt_Runtime.hpp
+++ src/Math/ModInt_Runtime.hpp
@@ -13,7 +13,7 @@ class Montgomery32 {};
 class Montgomery64 {};
 class Barrett {};
 class Barrett2 {};
-template <class Int, int id= -1> using ModInt_Runtime= conditional_t<is_same_v<Int, Montgomery32>, MInt<int, u32, RB<MP_Mo<u32, u64, 32, 31>, (1 << 30), id>>, conditional_t<is_same_v<Int, Montgomery64>, MInt<i64, u64, RB<MP_Mo<u64, u128, 64, 63>, (1ull << 62), id>>, conditional_t<is_same_v<Int, Barrett>, MInt<int, u32, RB<MP_Br, (1u << 31), id>>, conditional_t<is_same_v<Int, Barrett2>, MInt<i64, u64, RB<MP_Br2, (1ull << 41), id>>, conditional_t<disjunction_v<is_same<Int, i64>, is_same<Int, u64>>, MInt<i64, u64, RB<MP_D2B1, ULLONG_MAX, id>>, MInt<int, u32, RB<MP_Na, UINT_MAX, id>>>>>>>;
+template <class Int, int id= -1> using ModInt_Runtime= conditional_t<is_same_v<Int, Montgomery32>, MInt<int, u32, RB<MP_Mo<u32, u64, 32, 31>, (1 << 30), id>>, conditional_t<is_same_v<Int, Montgomery64>, MInt<i64, u64, RB<MP_Mo<u64, u128, 64, 63>, (1ull << 62), id>>, conditional_t<is_same_v<Int, Barrett>, MInt<int, u32, RB<MP_Br, (1u << 31), id>>, conditional_t<is_same_v<Int, Barrett2>, MInt<i64, u64, RB<MP_Br2, (1ull << 41), id>>, conditional_t<disjunction_v<is_same<Int, i64>, is_same<Int, u64>>, MInt<i64, u64, RB<MP_D2B1, u64(-1), id>>, MInt<int, u32, RB<MP_Na, u32(-1), id>>>>>>>;
 template <class T, enable_if_t<is_runtimemodint_v<T>, nullptr_t> = nullptr> constexpr u64 mv() { return T::max(); }
 }
 using math_internal::ModInt_Runtime, math_internal::Montgomery32, math_internal::Montgomery64, math_internal::Barrett, math_internal::Barrett2, math_internal::is_runtimemodint_v;

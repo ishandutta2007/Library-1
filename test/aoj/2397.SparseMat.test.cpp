6ac7348a827e268c2ba4560668395b8edf41c6ce
hashiryo <hashimotoryoma@gmail.com>
2023-08-04 15:19:28 +0900

    rm SparseMat

--- test/aoj/2397.SparseMat.test.cpp
+++ test/aoj/2397.SparseMat.test.cpp
@@ -2,7 +2,6 @@
 // 疎行列の行列累乗
 #include <iostream>
 #include <algorithm>
-#include "src/LinearAlgebra/SparseMatrix.hpp"
 #include "src/LinearAlgebra/MinimalPolynomial.hpp"
 #include "src/Math/ModInt.hpp"
 using namespace std;
@@ -10,28 +9,32 @@ signed main() {
  cin.tie(0);
  ios::sync_with_stdio(0);
  using Mint= ModInt<int(1e9 + 9)>;
+ using Vec= Vector<Mint>;
  long long W, H, N;
  for (int cnt= 0; cin >> W >> H >> N && W;) {
   pair<long long, int> obst[N];
   for (long long i= 0, x, y; i < N; i++) cin >> x >> y, obst[i]= make_pair(y - 1, x - 1);
   sort(obst, obst + N);
-  SparseMatrix<Mint> A(W, W);
-  for (int i= 0; i < W; i++) {
-   A.add_component(i, i, 1);
-   if (i) A.add_component(i, i - 1, 1);
-   if (i + 1 < W) A.add_component(i, i + 1, 1);
-  }
-  Vector<Mint> b(W);
+  auto f= [&](const Vec& v) {
+   Vec ret(W);
+   for (int i= 0; i < W; i++) {
+    ret[i]+= v[i];
+    if (i) ret[i]+= v[i - 1];
+    if (i + 1 < W) ret[i]+= v[i + 1];
+   }
+   return ret;
+  };
+  Vec b(W);
   b[0]= 1;
   long long y= 0;
   for (int i= 0; i < N; i++) {
-   b= MinimalPolynomial(A, b).pow(obst[i].first - y);
+   b= MinimalPolynomial(f, b).pow(obst[i].first - y);
    int j= i;
    while (j < N && obst[i].first == obst[j].first) b[obst[j++].second]= 0;
    i= j - 1;
    y= obst[i].first;
   }
-  b= MinimalPolynomial(A, b).pow(H - 1 - y);
+  b= MinimalPolynomial(f, b).pow(H - 1 - y);
   cout << "Case " << ++cnt << ": " << b[W - 1] << '\n';
  }
  return 0;
--- test/yukicoder/1750.SparseMat.test.cpp
+++ test/yukicoder/1750.SparseMat.test.cpp
@@ -2,24 +2,24 @@
 // 疎行列の行列累乗
 #include <iostream>
 #include "src/Math/ModInt.hpp"
-#include "src/LinearAlgebra/SparseMatrix.hpp"
 #include "src/LinearAlgebra/MinimalPolynomial.hpp"
 using namespace std;
 signed main() {
  cin.tie(0);
  ios::sync_with_stdio(false);
  using Mint= ModInt<998244353>;
+ using Vec= Vector<Mint>;
  long long N, M, T;
  cin >> N >> M >> T;
- SparseMatrix<Mint> mat(N, N);
- while (M--) {
-  int s, t;
-  cin >> s >> t;
-  mat.add_component(s, t, 1);
-  mat.add_component(t, s, 1);
- }
+ int s[M], t[M];
+ for (int i= 0; i < M; ++i) cin >> s[i] >> t[i];
+ auto f= [&](const Vec& v) {
+  Vec ret(N);
+  for (int i= 0; i < M; ++i) ret[s[i]]+= v[t[i]], ret[t[i]]+= v[t[i]];
+  return ret;
+ };
  Vector<Mint> vec(N);
  vec[0]= 1;
- cout << MinimalPolynomial(mat, vec).pow(T)[0] << '\n';
+ cout << MinimalPolynomial(f, vec).pow(T)[0] << '\n';
  return 0;
 }

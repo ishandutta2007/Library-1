35918500aad80f4e0e3bb3efb90d5e531d985a48
hashiryo <hashimotoryoma@gmail.com>
2023-08-05 18:38:55 +0900

    fix

--- src/Math/mod_inv.hpp
+++ src/Math/mod_inv.hpp
@@ -4,6 +4,6 @@
 template <class Int> constexpr inline Int mod_inv(Int a, Int mod) {
  static_assert(std::is_signed_v<Int>);
  Int x= 1, y= 0, b= mod;
- for (Int q= 0, z= 0, c= 0; b;) z= x, c= a, x= y, y= z - y * (q= a / b), a= b, b= c - b * q;
+ for (Int q= 0, z= 0; b;) z= x, x= y, y= z - y * (q= a / b), z= a, a= b, b= z - b * q;
  return assert(a == 1), x < 0 ? mod - (-x) % mod : x % mod;
 }

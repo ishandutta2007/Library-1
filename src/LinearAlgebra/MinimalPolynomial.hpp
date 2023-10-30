8a43de886f0d40a190eb8eb4123e98b9953b1724
hashiryo <hashimotoryoma@gmail.com>
2023-10-30 17:57:49 +0900

    fix

--- src/LinearAlgebra/MinimalPolynomial.hpp
+++ src/LinearAlgebra/MinimalPolynomial.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <cassert>
 #include "src/Internal/modint_traits.hpp"
 #include "src/Math/berlekamp_massey.hpp"
 #include "src/LinearAlgebra/Vector.hpp"
---
+++ test/yukicoder/2114.test.cpp
@@ -0,0 +1,47 @@
+#define PROBLEM "https://yukicoder.me/problems/no/2114"
+#include <iostream>
+#include <unordered_map>
+#include "src/Optimization/PiecewiseLinearConvexfunction.hpp"
+#include "src/Misc/compress.hpp"
+using namespace std;
+signed main() {
+ cin.tie(0);
+ ios::sync_with_stdio(0);
+ using PLCF= PiecewiseLinearConvexfunction<>;
+ int N, M, K;
+ cin >> N >> M >> K;
+ unordered_map<int, array<vector<int>, 2>> mp;
+ for (int i= 0; i < N; ++i) {
+  int B;
+  cin >> B;
+  mp[B % K][N >= M].push_back(B / K);
+ }
+ for (int i= 0; i < M; ++i) {
+  int R;
+  cin >> R;
+  mp[R % K][N < M].push_back(R / K);
+ }
+ long long ans= 0;
+ for (auto &&[_, arr]: mp) {
+  auto &&[A, B]= arr;
+  if (A.size() > B.size()) return cout << -1 << '\n', 0;
+  vector<int> vec(A);
+  for (int i: B) vec.push_back(i);
+  auto id= compress(vec);
+  int n= vec.size();
+  vector<int> a(n), b(n);
+  for (int i: A) ++a[id(i)];
+  for (int i: B) ++b[id(i)];
+  PLCF f;
+  f.chinfty_left();
+  for (int i= 0; i < n; ++i) {
+   f.cumulative_chmin();
+   f.shift(a[i] - b[i]);
+   if (i < n - 1) f.add_abs(vec[i + 1] - vec[i], 0);
+  }
+  ans+= f.eval(0);
+  PLCF::clear();
+ }
+ cout << ans << '\n';
+ return 0;
+}

48163e5c24b96a4b73d721a1c6820b09a73c565d
hashiryo <hashimotoryoma@gmail.com>
2023-11-13 12:47:03 +0900

    modify

--- md/String/RollingHash.md
+++ md/String/RollingHash.md
@@ -4,4 +4,7 @@ documentation_of: ../../src/String/RollingHash.hpp
 ---
 `+ - *` が定義されているクラスでhashを計算
 ## 問題例
-[AtCoder Beginner Contest 274 Ex - XOR Sum of Arrays](https://atcoder.jp/contests/abc274/tasks/abc274_h) (標数2の体 e.g. Nimber)
+[AtCoder Regular Contest 055 C - ABCAC](https://atcoder.jp/contests/arc055/tasks/arc055_c) \
+[AtCoder Beginner Contest 274 Ex - XOR Sum of Arrays](https://atcoder.jp/contests/abc274/tasks/abc274_h) (標数2の体 e.g. Nimber)\
+[AtCoder Beginner Contest 284 F - ABCBAC](https://atcoder.jp/contests/abc284/tasks/abc284_f) (sp judge)\
+[競プロ典型 90 問 047 - Monochromatic Diagonal（★7）](https://atcoder.jp/contests/typical90/tasks/typical90_au)
---
+++ test/atcoder/abc150_f.RH.Nimber.test.cpp
@@ -0,0 +1,28 @@
+#define PROBLEM "https://atcoder.jp/contests/abc150/tasks/abc150_f"
+#include <iostream>
+#include <vector>
+#include "src/String/RollingHash.hpp"
+#include "src/Misc/rng.hpp"
+#include "src/Math/Nimber.hpp"
+using namespace std;
+signed main() {
+ cin.tie(0);
+ ios::sync_with_stdio(0);
+ Nimber::init();
+ using RH= RollingHash<Nimber>;
+ Nimber base= rng(2, 1ll << 60);
+ RH::set_base(base);
+ int N;
+ cin >> N;
+ vector<Nimber> a(N), b(N);
+ for (int i= 0; i < N; ++i) cin >> a[i];
+ for (int i= 0; i < N; ++i) cin >> b[i];
+ RH ra(a), rb(b);
+ auto X= (base.pow(N) - 1) / (base - 1);
+ auto bh= rb.get_hash();
+ for (int k= 0; k < N; ++k) {
+  auto x= a[k] + b[0];
+  if (concat_hash(ra.sub(k, N), ra.sub(0, k)) + x * X == bh) cout << k << " " << x << '\n';
+ }
+ return 0;
+}

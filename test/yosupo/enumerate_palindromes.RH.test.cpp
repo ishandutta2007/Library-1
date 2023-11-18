ef5360f8a65aba177988d1dd7974c91d5275dc3b
hashiryo <hashimotoryoma@gmail.com>
2023-11-18 10:50:28 +0900

    add test

---
+++ test/yosupo/enumerate_palindromes.RH.test.cpp
@@ -0,0 +1,28 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/enumerate_palindromes"
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include "src/Math/ModInt.hpp"
+#include "src/Math/CartesianProduct.hpp"
+#include "src/String/RollingHash.hpp"
+#include "src/Misc/rng.hpp"
+using namespace std;
+signed main() {
+ cin.tie(0);
+ ios::sync_with_stdio(0);
+ using Mint= ModInt<998244353>;
+ using K= CartesianProduct<Mint, Mint>;
+ using RH= RollingHash<K>;
+ RH::init({rng(), rng()});
+ string S;
+ cin >> S;
+ int N= S.length();
+ auto Srv= S;
+ reverse(Srv.begin(), Srv.end());
+ RH rh(S), rhrv(Srv);
+ for (int i= 0; i < N + N - 1; ++i) {
+  int n= lcp(rh.sub((i + 1) / 2), rhrv.sub((N + N - i - 1) / 2));
+  cout << n * 2 - !(i & 1) << " \n"[i == N + N - 2];
+ }
+ return 0;
+}

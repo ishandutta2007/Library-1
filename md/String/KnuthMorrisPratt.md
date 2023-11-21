579579432dfc27e7cb8160c1c6a315e2915502a1
hashiryo <hashimotoryoma@gmail.com>
2023-11-21 15:50:22 +0900

    merge kmp to z

--- md/String/KnuthMorrisPratt.md
+++
@@ -1,9 +0,0 @@
----
-title: Knuth-Morris-Pratt
-documentation_of: ../../src/String/KnuthMorrisPratt.hpp
----
-## 計算量
-テーブル構築 $\mathcal{O}(|S|)$ \
-pattern_match(T) $T$の中から$S$と一致する部分を検索 $\mathcal{O}(|T|)$
-## 問題例
-[AtCoder Regular Contest 060 F - 最良表現](https://atcoder.jp/contests/arc060/tasks/arc060_d) (`period(i)`)
--- test/aoj/ALDS1_14_B.zalgo.test.cpp
+++ test/aoj/ALDS1_14_B.zalgo.test.cpp
@@ -1,15 +1,17 @@
 #define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/lesson/1/ALDS1/14/ALDS1_14_B"
 #include <iostream>
 #include <string>
-#include "src/String/KnuthMorrisPratt.hpp"
+#include "src/String/z_algorithm.hpp"
 using namespace std;
 signed main() {
  cin.tie(0);
  ios::sync_with_stdio(0);
  string T, P;
  cin >> T >> P;
- KnuthMorrisPratt KMP(P);
- auto ans= KMP.pattern_match(T);
- for (auto a: ans) cout << a << "\n";
+ int n= P.size(), m= T.size();
+ P+= '$' + T;
+ auto Z= z_algorithm(P);
+ for (int i= n + 1; i <= m + n; ++i)
+  if (Z[i] == n) cout << i - n - 1 << '\n';
  return 0;
 }

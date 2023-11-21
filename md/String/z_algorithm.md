e33e87f2b61a38587a4cd90f254b9d8102718b97
hashiryo <hashimotoryoma@gmail.com>
2023-11-21 13:51:34 +0900

    modify

--- md/String/z_algorithm.md
+++ md/String/z_algorithm.md
@@ -10,5 +10,5 @@ documentation_of: ../../src/String/z_algorithm.hpp
 
 |関数|概要|計算量|
 |---|---|---|
-|`z_algorithm(S)`| 長さ $n$ の配列 $Z$ を返す. <br> $Z_k := $ $S$ と $S_{k:}$ の最長共通接頭辞の長さ. <br> 引数は `vector<T>` か `string` を渡せる.|$\mathcal{O}(n)$|
-|`run_enumerate(S)`|文字列 $S$ の [**連 (run)**](https://www.iss.is.tohoku.ac.jp/stringology/Algorithms/Basic/run.html) を列挙する. <br> タプル $(t,l,r)$ で部分文字列 $S_{l:r}$ の最小周期が $t$ であることを表現する. <br> 返り値は `vector<tuple<int,int,int>>`. <br> 引数は `vector<Int>` か `string` を渡せる.|$\mathcal{O}(n\log n)$|
+|`z_algorithm(S)`| 長さ $n$ の配列 $Z$ を返す. <br> $Z_k := $ $S$ と $S_{k:}$ の最長共通接頭辞の長さ. <br> 引数は `vector<T>` か `string` を想定.|$\mathcal{O}(n)$|
+|`run_enumerate(S)`|文字列 $S$ の [**連 (run)**](https://www.iss.is.tohoku.ac.jp/stringology/Algorithms/Basic/run.html) を列挙する. <br> タプル $(t,l,r)$ で部分文字列 $S_{l:r}$ の最小周期が $t$ であることを表現する. <br> 返り値は `vector<tuple<int,int,int>>`. <br> 引数は `vector<T>` か `string` を想定.|$\mathcal{O}(n\log n)$|
--- src/String/z_algorithm.hpp
+++ src/String/z_algorithm.hpp
@@ -3,7 +3,7 @@
 #include <algorithm>
 #include <string>
 #include <tuple>
-template <class T> std::vector<int> z_algorithm(const std::vector<T> &s) {
+template <class String> std::vector<int> z_algorithm(const String &s) {
  const int n= s.size();
  if (n == 0) return {};
  std::vector<int> z(n);
@@ -14,7 +14,6 @@ template <class T> std::vector<int> z_algorithm(const std::vector<T> &s) {
  }
  return z[0]= n, z;
 }
-std::vector<int> z_algorithm(const std::string &s) { return z_algorithm(std::vector<int>(s.begin(), s.end())); }
 namespace string_internal {
 template <class String> auto run_enumerate_(const String &s, typename String::value_type a= 0) {
  using Run= std::tuple<int, int, int>;
@@ -30,52 +29,52 @@ template <class String> auto run_enumerate_(const String &s, typename String::va
   std::vector<int> zrl_r= z_algorithm(rl);
   const int sz= rl.size();
   std::vector<Run> ret;
-  auto push= [&](int p, int b, int e) { (b == l || e == r ? ret : glb).emplace_back(p, b, e); };
+  auto push= [&](int b, int e, int p) { (b == l || e == r ? ret : glb).emplace_back(b, e, p); };
   std::vector<Run> run_m(rsz / 2 + 1);
-  for (auto [p, b, e]: run_r) {
-   if (b != m) ret.emplace_back(p, b, e);
-   else run_m[p]= Run{p, b, e};
+  for (auto [b, e, p]: run_r) {
+   if (b != m) ret.emplace_back(b, e, p);
+   else run_m[p]= Run{b, e, p};
   }
-  for (auto [p, b, e]: run_l)
-   if (e != m) ret.emplace_back(p, b, e);
+  for (auto [b, e, p]: run_l)
+   if (e != m) ret.emplace_back(b, e, p);
    else if (zrl[sz - p] == p) {
-    if (std::get<0>(run_m[p])) push(p, b, std::get<2>(run_m[p])), run_m[p]= Run{};
-    else push(p, b, m + p + zrl[p]);
-   } else push(p, b, m + zrl[sz - p]);
-  for (auto [p, b, e]: run_m)
+    if (std::get<2>(run_m[p])) push(b, std::get<1>(run_m[p]), p), run_m[p]= Run{};
+    else push(b, m + p + zrl[p], p);
+   } else push(b, m + zrl[sz - p], p);
+  for (auto [b, e, p]: run_m)
    if (p) {
-    if (zrl[sz - p] != p) push(p, m - zrl_r[sz - p], e);
-    else if (2 * p > lsz || zrl[sz - 2 * p] < p) push(p, m - p - zrl_r[p], e);
+    if (zrl[sz - p] != p) push(m - zrl_r[sz - p], e, p);
+    else if (2 * p > lsz || zrl[sz - 2 * p] < p) push(m - p - zrl_r[p], e, p);
    }
   for (int p= 1; p <= lsz; ++p)
    if (bool skpr= 2 * p <= rsz && zrl[p] >= p, skpl= 2 * p <= lsz && zrl[sz - 2 * p] >= p; zrl[sz - p] == p) {
-    if (!skpl && !skpr) push(p, m - p - zrl_r[p], m + p + zrl[p]);
+    if (!skpl && !skpr) push(m - p - zrl_r[p], m + p + zrl[p], p);
    } else {
     if (!skpr)
-     if (int b= m - zrl_r[sz - p], e= m + p + zrl[p]; e - b >= 2 * p) push(p, b, e);
+     if (int b= m - zrl_r[sz - p], e= m + p + zrl[p]; e - b >= 2 * p) push(b, e, p);
     if (!skpl)
-     if (int b= m - p - zrl_r[p], e= m + zrl[sz - p]; e - b >= 2 * p) push(p, b, e);
+     if (int b= m - p - zrl_r[p], e= m + zrl[sz - p]; e - b >= 2 * p) push(b, e, p);
    }
   return ret;
  };
  std::vector<std::tuple<int, int, int>> runs= rec(rec, 0, s.size());
- std::copy(glb.begin(), glb.end(), std::back_inserter(runs));
+ std::copy(glb.begin(), glb.end(), std::back_inserter(runs)), std::sort(runs.begin(), runs.end()), runs.erase(std::unique(runs.begin(), runs.end(), [](auto &r1, auto &r2) { return std::get<0>(r1) == std::get<0>(r2) && std::get<1>(r1) == std::get<1>(r2); }), runs.end());
  int rn= runs.size(), n= s.size();
  glb.resize(rn);
  std::vector<int> pt(n);
-#define RADIX(A, B, C, D, E) \
- for (auto [p, l, r]: A) ++pt[C]; \
- for (int i= 1; E < n; ++i) pt[i]+= pt[i - 1]; \
- for (int i= rn; i--;) B[--pt[D]]= A[i];
- RADIX(runs, glb, r - 1, std::get<2>(runs[i]) - 1, i)
- pt.assign(n, 0);
- RADIX(glb, runs, l, std::get<1>(glb[i]), i)
- runs.erase(std::unique(runs.begin(), runs.end(), [](auto &r1, auto &r2) { return std::get<1>(r1) == std::get<1>(r2) && std::get<2>(r1) == std::get<2>(r2); }), runs.end()), glb.resize(rn= runs.size()), pt.assign(n / 2, 0);
- RADIX(runs, glb, p - 1, std::get<0>(runs[i]) - 1, i + i)
-#undef RADIX
+ for (auto [l, r, p]: runs) ++pt[p];
+ for (int i= 1; i < n; ++i) pt[i]+= pt[i - 1];
+ for (int i= rn; i--;) glb[--pt[std::get<2>(runs[i])]]= {std::get<2>(runs[i]), std::get<0>(runs[i]), std::get<1>(runs[i])};
  return glb;
 }
-template <class Int> auto run_enumerate(const std::vector<Int> &s) { return run_enumerate_(s, *std::max_element(s.begin(), s.end()) + 1); }
+template <class Int, typename= std::enable_if_t<std::is_integral_v<Int>>> auto run_enumerate(const std::vector<Int> &s) { return run_enumerate_(s, *std::max_element(s.begin(), s.end()) + 1); }
+template <class String> auto run_enumerate(const String &s) {
+ auto v= s;
+ std::sort(v.begin(), v.end()), v.erase(std::unique(v.begin(), v.end()), v.end());
+ std::vector<int> t(s.size());
+ for (int i= s.size(); i--;) t[i]= std::lower_bound(v.begin(), v.end(), s[i]) - v.begin();
+ return run_enumerate_(t, v.size() + 1);
+}
 auto run_enumerate(const std::string &s) { return run_enumerate_(s); }
 }
 using string_internal::run_enumerate;

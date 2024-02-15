bcd76d16fc012744d28c22c26d13fd907ee3341a
hashiryo <hashimotoryoma@gmail.com>
2024-02-15 23:40:55 +0900

    renew scc

--- src/Graph/StronglyConnectedComponents.hpp
+++ src/Graph/StronglyConnectedComponents.hpp
@@ -1,54 +1,43 @@
 #pragma once
 #include <algorithm>
-#include <numeric>
-#include <array>
-#include "src/DataStructure/CsrArray.hpp"
+#include "src/Graph/Graph.hpp"
 class StronglyConnectedComponents {
- std::vector<std::array<int, 2>> es;
- std::vector<int> csr, pos, id;
+ Graph _g;
+ std::vector<int> csr, pos, ord;
 public:
- StronglyConnectedComponents(int n): csr(n, -2), id(n) {}
- void add_edge(int src, int dst) { es.push_back({src, dst}); }
- void build() {
-  const int n= id.size();
-  std::vector<int> g(es.size()), sep(n + 1), ord(n);
-  for (auto [s, d]: es) ++sep[s];
-  for (int i= 0; i < n; ++i) sep[i + 1]+= sep[i];
-  for (auto [s, d]: es) g[--sep[s]]= d;
-  std::vector<int> dat(sep.begin(), sep.begin() + n);
-  int k= n, p;
-  for (int s= 0; s < n; ++s)
-   if (csr[s] == -2)
-    for (csr[p= s]= -1; p >= 0;) {
-     if (dat[p] == sep[p + 1]) ord[--k]= p, p= csr[p];
-     else if (int q= g[dat[p]++]; csr[q] == -2) csr[q]= p, p= q;
-    }
-  sep.assign(n + 1, 0), pos= {p= 0};
-  for (auto [s, d]: es) ++sep[d];
-  for (int i= 0; i < n; ++i) sep[i + 1]+= sep[i];
-  for (auto [s, d]: es) g[--sep[d]]= s;
+ StronglyConnectedComponents(const Graph &g): _g(g) {
+  const int n= g.vertex_size();
+  csr.assign(n, -2), ord.resize(n), pos= {0};
+  {
+   auto adj= g.adjacency_vertex(1);
+   std::vector<int> ei(adj.p.begin(), adj.p.begin() + n);
+   for (int s= 0, k= n, p; s < n; ++s)
+    if (csr[s] == -2)
+     for (csr[p= s]= -1; p >= 0;)
+      if (ei[p] == adj.p[p + 1]) ord[--k]= p, p= csr[p];
+      else if (int q= adj.dat[ei[p]++]; csr[q] == -2) csr[q]= p, p= q;
+  }
+  for (auto &[s, d]: _g) std::swap(s, d);
+  auto adj= _g.adjacency_vertex(1);
+  std::vector<char> vis(n);
+  int k= 0, p= 0;
   for (int s: ord)
-   if (dat[s] >= 0) {
-    for (csr[k++]= s, dat[s]= -1; p < k; ++p)
-     for (int v= csr[p], j= sep[v], u; j < sep[v + 1]; ++j)
-      if (dat[u= g[j]] >= 0) dat[u]= -1, csr[k++]= u;
+   if (!vis[s]) {
+    for (vis[csr[k++]= s]= 1; p < k; ++p)
+     for (int u: adj[csr[p]])
+      if (!vis[u]) vis[csr[k++]= u]= 1;
     pos.push_back(k);
    }
   for (int i= pos.size() - 1; i--;)
-   while (k > pos[i]) id[csr[--k]]= i;
+   while (k > pos[i]) ord[csr[--k]]= i;
  }
- int components_num() const { return pos.size() - 1; }
+ size_t size() const { return pos.size() - 1; }
  ConstListRange<int> block(int k) const { return {csr.cbegin() + pos[k], csr.cbegin() + pos[k + 1]}; }
- int belong(int i) const { return id[i]; }
- CsrArray<int> dag() const {
-  std::vector<std::array<int, 2>> es_;
-  for (auto [s, d]: es)
-   if (s= id[s], d= id[d]; s != d) es_.push_back({s, d});
-  std::sort(es_.begin(), es_.end()), es_.erase(std::unique(es_.begin(), es_.end()), es_.end());
-  std::vector<int> g(es_.size()), p(pos.size());
-  for (auto [s, d]: es_) ++p[s];
-  std::partial_sum(p.begin(), p.end(), p.begin());
-  for (auto [s, d]: es_) g[--p[s]]= d;
-  return {g, p};
+ int operator()(int i) const { return ord[i]; }
+ Graph dag() const {
+  Graph ret(size());
+  for (auto [d, s]: _g)
+   if (int u= (*this)(s), v= (*this)(d); u != v) ret.add_edge(u, v);
+  return sort(ret.begin(), ret.end()), ret.erase(unique(ret.begin(), ret.end()), ret.end()), ret;
  }
 };
--- test/yukicoder/1813.test.cpp
+++ test/yukicoder/1813.test.cpp
@@ -1,6 +1,7 @@
 #define PROBLEM "https://yukicoder.me/problems/no/1813"
 #include <iostream>
 #include <algorithm>
+#include "src/Graph/Graph.hpp"
 #include "src/Graph/StronglyConnectedComponents.hpp"
 using namespace std;
 signed main() {
@@ -8,27 +9,20 @@ signed main() {
  ios::sync_with_stdio(0);
  int N, M;
  cin >> N >> M;
- StronglyConnectedComponents scc(N);
- for (int i= 0; i < M; ++i) {
-  int A, B;
-  cin >> A >> B;
-  scc.add_edge(--A, --B);
+ Graph g(N, M);
+ for (int i= 0; i < M; ++i) cin >> g[i], --g[i];
+ StronglyConnectedComponents scc(g);
+ int C= scc.size();
+ if (C == 1) return cout << 0 << '\n', 0;
+ auto dag= scc.dag().adjacency_vertex(1);
+ int cnt[2]= {0, 0};
+ bool st[C];
+ fill_n(st, C, true);
+ for (int i= 0; i < C; ++i) {
+  if (st[i]) ++cnt[0];
+  for (int j: dag[i]) st[j]= false;
+  if (!dag[i].size()) ++cnt[1];
  }
- scc.build();
- int C= scc.components_num();
- if (C == 1) {
-  cout << 0 << '\n';
- } else {
-  auto dag= scc.dag();
-  int cnt[2]= {0, 0};
-  bool st[C];
-  fill_n(st, C, true);
-  for (int i= 0; i < C; ++i) {
-   if (st[i]) ++cnt[0];
-   for (int j: dag[i]) st[j]= false;
-   if (!dag[i].size()) ++cnt[1];
-  }
-  cout << max(cnt[0], cnt[1]) << '\n';
- }
+ cout << max(cnt[0], cnt[1]) << '\n';
  return 0;
 }

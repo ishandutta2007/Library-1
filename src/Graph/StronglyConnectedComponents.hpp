619ffd96e3fdd132f1b636c603238f825412cc85
hashiryo <hashimotoryoma@gmail.com>
2024-02-18 23:57:57 +0900

    modify scc

--- src/Graph/StronglyConnectedComponents.hpp
+++ src/Graph/StronglyConnectedComponents.hpp
@@ -2,41 +2,41 @@
 #include <algorithm>
 #include "src/Graph/Graph.hpp"
 class StronglyConnectedComponents {
- std::vector<int> csr, pos, ord;
+ std::vector<int> m, q, b;
 public:
  StronglyConnectedComponents(const Graph &g) {
   const int n= g.vertex_size();
-  csr.assign(n, -2), ord.resize(n), pos= {0};
+  m.assign(n, -2), b.resize(n), q= {0};
   {
    auto adj= g.adjacency_vertex(1);
-   std::vector<int> ei(adj.p.begin(), adj.p.begin() + n);
+   std::vector<int> c(adj.p.begin(), adj.p.begin() + n);
    for (int s= 0, k= n, p; s < n; ++s)
-    if (csr[s] == -2)
-     for (csr[p= s]= -1; p >= 0;) {
-      if (ei[p] == adj.p[p + 1]) ord[--k]= p, p= csr[p];
-      else if (int q= adj.dat[ei[p]++]; csr[q] == -2) csr[q]= p, p= q;
+    if (m[s] == -2)
+     for (m[p= s]= -1; p >= 0;) {
+      if (c[p] == adj.p[p + 1]) b[--k]= p, p= m[p];
+      else if (int w= adj.dat[c[p]++]; m[w] == -2) m[w]= p, p= w;
      }
   }
   auto adj= g.adjacency_vertex(-1);
-  std::vector<char> vis(n);
+  std::vector<char> z(n);
   int k= 0, p= 0;
-  for (int s: ord)
-   if (!vis[s]) {
-    for (vis[csr[k++]= s]= 1; p < k; ++p)
-     for (int u: adj[csr[p]])
-      if (!vis[u]) vis[csr[k++]= u]= 1;
-    pos.push_back(k);
+  for (int s: b)
+   if (!z[s]) {
+    for (z[m[k++]= s]= 1; p < k; ++p)
+     for (int u: adj[m[p]])
+      if (!z[u]) z[m[k++]= u]= 1;
+    q.push_back(k);
    }
-  for (int i= pos.size() - 1; i--;)
-   while (k > pos[i]) ord[csr[--k]]= i;
+  for (int i= q.size() - 1; i--;)
+   while (k > q[i]) b[m[--k]]= i;
  }
- size_t size() const { return pos.size() - 1; }
- ConstListRange<int> block(int k) const { return {csr.cbegin() + pos[k], csr.cbegin() + pos[k + 1]}; }
- int operator()(int i) const { return ord[i]; }
+ size_t size() const { return q.size() - 1; }
+ ConstListRange<int> block(int k) const { return {m.cbegin() + q[k], m.cbegin() + q[k + 1]}; }
+ int operator()(int i) const { return b[i]; }
  Graph dag(const Graph &g) const {
   Graph ret(size());
   for (auto [s, d]: g)
-   if (int u= (*this)(s), v= (*this)(d); u != v) ret.add_edge(u, v);
-  return sort(ret.begin(), ret.end()), ret.erase(unique(ret.begin(), ret.end()), ret.end()), ret;
+   if (int u= b[s], v= b[d]; u != v) ret.add_edge(u, v);
+  return std::sort(ret.begin(), ret.end()), ret.erase(std::unique(ret.begin(), ret.end()), ret.end()), ret;
  }
 };

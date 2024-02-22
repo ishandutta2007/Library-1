5fdc4129623db03f7a8a6f7e47be0625fd56672f
hashiryo <hashimotoryoma@gmail.com>
2024-02-23 00:06:46 +0900

    add test

---
+++ test/yukicoder/1569.Nimber.test.cpp
@@ -0,0 +1,17 @@
+#define PROBLEM "https://yukicoder.me/problems/no/1569"
+#include <iostream>
+#include <vector>
+#include "src/Math/Nimber.hpp"
+#include "src/Math/bostan_mori.hpp"
+using namespace std;
+signed main() {
+ cin.tie(0);
+ ios::sync_with_stdio(0);
+ Nimber::init();
+ long long N, K;
+ cin >> N >> K;
+ vector<Nimber> A(N);
+ for (int i= 0; i < N; ++i) cin >> A[i];
+ cout << linear_recurrence(vector<Nimber>(N, 1), A, K - 1) << '\n';
+ return 0;
+}

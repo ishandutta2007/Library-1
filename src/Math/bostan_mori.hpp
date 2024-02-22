03207da9d0326ee36cba28e3ddd76deb2df6b569
hashiryo <hashimotoryoma@gmail.com>
2024-02-22 16:45:01 +0900

    add test

--- src/Math/bostan_mori.hpp
+++ src/Math/bostan_mori.hpp
@@ -3,27 +3,35 @@
 #include <cassert>
 #include <cstdint>
 template <class K> K div_at(std::vector<K> p, std::vector<K> q, uint64_t k) {
- unsigned n= p.size() - 1, m= q.size() - 1, nn, j;
+ int n= p.size() - 1, m= q.size() - 1;
  for (assert(q[0] != K(0));; --n)
   if (n < 0 || p[n] != K()) break;
  for (;; --m)
   if (m < 0 || q[m] != K()) break;
- const unsigned l= std::max(n, m) + 1;
- std::vector<K> tmp(l);
- for (p.resize(l), q.resize(l); k > m; q.swap(p), p.swap(tmp)) {
-  for (tmp.assign((nn= (n + m - ((n ^ m ^ k) & 1)) >> 1) + 1, K()), j= 0; j <= m; j+= 2)
-   for (unsigned i= k & 1; i <= n; i+= 2) tmp[(i + j) >> 1]+= p[i] * q[j];
-  for (j= 1; j <= m; j+= 2)
-   for (unsigned i= (~k) & 1; i <= n; i+= 2) tmp[(i + j) >> 1]-= p[i] * q[j];
-  for (p.assign(m + 1, K()), j= 2; j <= m; j+= 2)
-   for (int i= j; (i-= 2) >= 0;) p[(i + j) >> 1]+= q[i] * q[j];
-  for (k>>= 1, n= nn, j= 3; j <= m; j+= 2)
-   for (int i= j; (i-= 2) >= 0;) p[(i + j) >> 1]-= q[i] * q[j];
-  for (int i= m + 1; i--;) p[i]+= p[i];
-  for (unsigned i= 0; i <= m; i+= 2) p[i]+= q[i] * q[i];
-  for (unsigned i= 1; i <= m; i+= 2) p[i]-= q[i] * q[i];
+ const int l= std::max(n, m) + 1;
+ p.resize(l), q.resize(l);
+ for (std::vector<K> np; k > m; q.swap(p), p.swap(np), k>>= 1) {
+  np.assign(l, K());
+  if (k & 1) {
+   for (int i= 0; i < l; i+= 2)
+    for (int j= 1; j < l; j+= 2) np[(i + j) >> 1]+= p[j] * q[i] - p[i] * q[j];
+  } else {
+   for (int i= 0; i < l; i+= 2)
+    for (int j= 0; j < l; j+= 2) np[(i + j) >> 1]+= p[i] * q[j];
+   for (int i= 1; i < l; i+= 2)
+    for (int j= 1; j < l; j+= 2) np[(i + j) >> 1]-= p[i] * q[j];
+  }
+  p.assign(l, K());
+  for (int i= 0; i < l; i+= 2)
+   for (int j= 0; j < i; j+= 2) p[(i + j) >> 1]+= q[i] * q[j];
+  for (int i= 1; i < l; i+= 2)
+   for (int j= 1; j < i; j+= 2) p[(i + j) >> 1]-= q[i] * q[j];
+  for (int i= l; i--;) p[i]+= p[i];
+  for (int i= 0; i < l; i+= 2) p[i]+= q[i] * q[i];
+  for (int i= 1; i < l; i+= 2) p[i]-= q[i] * q[i];
  }
- for (K iv= K(1) / q[j= 0]; j <= k; p[j++]*= iv)
+ K iv= K(1) / q[0];
+ for (int j= 0; j <= k; p[j++]*= iv)
   for (int i= j; i; --i) p[j]-= p[j - i] * q[i];
  return p[k];
 }

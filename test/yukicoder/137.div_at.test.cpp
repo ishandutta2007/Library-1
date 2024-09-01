fa945c140eaa007dc63702c5ca300a8809c35ea6
hashiryo <hashimotoryoma@gmail.com>
2024-09-01 16:20:42 +0900

    fix

--- test/yukicoder/137.div_at.test.cpp
+++ test/yukicoder/137.div_at.test.cpp
@@ -18,7 +18,7 @@ signed main() {
   int A;
   cin >> A;
   std::vector<Mint> g(A + 1);
-  g[0]= 1, g[A]= -1, f= convolve(f, g);
+  g[0]= 1, g[A]= -1, f= convolve<Mint, 1 << 20>(f, g);
  }
  cout << div_at<Mint, 1 << 20>({1}, f, M) << '\n';
  return 0;

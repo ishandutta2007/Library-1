0e42b24fd08c6c20954bebe09c797193796ac59f
hashiryo <hashimotoryoma@gmail.com>
2024-09-01 11:34:20 +0900

    add ml

--- test/atcoder/abc117_d.test.cpp
+++ test/atcoder/abc117_d.test.cpp
@@ -1,5 +1,6 @@
 // competitive-verifier: PROBLEM https://atcoder.jp/contests/abc117/tasks/abc117_d
 // competitive-verifier: TLE 0.5
+// competitive-verifier: MLE 64
 #include <iostream>
 #include <vector>
 #include "src/Misc/Automaton.hpp"

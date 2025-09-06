3d716845cedcd9ff0b897de77cb0ad01f753cc95
hashiryo <hashimotoryoma@gmail.com>
2025-09-07 02:07:16 +0900

    modify tetration

--- src/Math/mod_tetration.hpp
+++ src/Math/mod_tetration.hpp
@@ -2,17 +2,17 @@
 #include <cstdint>
 #include "src/NumberTheory/Factors.hpp"
 namespace math_internal {
-constexpr u64 rec(u64 a, u64 b, u64 m) {
+constexpr u64 mod_tetration(u64 a, u64 b, u64 m) {
+ if (m == 1) return 0;
  if (a == 0) return (b ^ 1) & 1;
- if (b == 0 || m == 1) return 1;
- u64 ret= 1, k= 1, tmp= 1, i= 0;
- for (const auto [p, e]: Factors(m)) {
-  for (tmp= p - 1, i= e - (p == 2 && e > 3); --i;) tmp*= p;
-  k*= tmp / binary_gcd(k, tmp);
- }
- auto mod= [m](u128 x) { return x < m ? x : x % m + m; };
- for (k= rec(a, b - 1, k), a= mod(a);; a= mod(u128(a) * a))
-  if (k& 1 ? ret= mod(u128(ret) * a) : 0; !(k>>= 1)) return ret;
+ if (b == 0) return 1;
+ if (b == 1) return a % m;
+ u64 l= 1;
+ for (auto [p, e]: Factors(m))
+  for (l*= (p - 1) / binary_gcd(l, p - 1), e-= (p == 2 && e > 3); --e;) l*= p;
+ if (b= b == 2 ? a : mod_tetration(a, b - 1, l); !b) b= l;
+ for (l= 1;; a= (u128(a) * a) % m)
+  if (b& 1 ? l= (u128(l) * a) % m : 0; !(b>>= 1)) return l;
 }
 }
-constexpr uint64_t mod_tetration(uint64_t a, uint64_t b, uint64_t m) { return (a= math_internal::rec(a, b, m)) >= m ? a - m : a; }
+using math_internal::mod_tetration;

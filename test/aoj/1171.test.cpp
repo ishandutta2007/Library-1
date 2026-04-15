a06871d1fcac3875dbacc6c5ad2b0e1e832e2e0c
hashiryo <hashimotoryoma@gmail.com>
2026-04-15 11:38:46 +0900

    誤差を問題に合わせる

--- test/aoj/1171.test.cpp
+++ test/aoj/1171.test.cpp
@@ -1,5 +1,5 @@
 // competitive-verifier: PROBLEM https://onlinejudge.u-aizu.ac.jp/problems/1171
-// competitive-verifier: ERROR 0.00001
+// competitive-verifier: ERROR 0.001
 // competitive-verifier: TLE 0.5
 // competitive-verifier: MLE 64
 #include <iostream>
